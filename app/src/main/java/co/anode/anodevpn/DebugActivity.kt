3c50ad1bd61f8b7e732cc3cbb66f59111ce6d261
dkoukoul <dimitris.koukoulakis@gmail.com>
2020-06-25 14:15:00 +0300

    update debug activity

--- app/src/main/java/co/anode/anodevpn/DebugActivity.kt
+++ app/src/main/java/co/anode/anodevpn/DebugActivity.kt
@@ -2,9 +2,11 @@ package co.anode.anodevpn
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import java.net.URL
 
 class DebugActivity : AppCompatActivity() {
     @SuppressLint("SetTextI18n")
@@ -24,6 +26,8 @@ class DebugActivity : AppCompatActivity() {
         val username = findViewById<TextView>(R.id.text_username)
         val prefs = getSharedPreferences("co.anode.AnodeVPN", Context.MODE_PRIVATE)
         username.text = "Username: "+prefs.getString("username","")
+        val node = findViewById<TextView>(R.id.text_node)
+        node.text = "Last Node: "+prefs.getString("ServerPublicKey","")
         val peerstats = findViewById<TextView>(R.id.text_peerstats)
         val peers:ArrayList<Benc.Bdict> = CjdnsSocket.InterfaceController_peerStats()
         var info = ""
@@ -31,5 +35,27 @@ class DebugActivity : AppCompatActivity() {
             info += peers[i].toString()
         }
         peerstats.text = "Peer stats: $info"
+        val publicip = findViewById<TextView>(R.id.text_publicIP)
+        publicip.text = "Public IP: Retrieving ip..."
+        GetPublicIP().execute(publicip)
+    }
+
+    class GetPublicIP(): AsyncTask<TextView, Void, String>() {
+        private var ipText:TextView? = null
+
+        override fun doInBackground(vararg params: TextView?): String {
+            ipText = params[0]
+            return try {
+                URL("https://api.ipify.org").readText(Charsets.UTF_8)
+            } catch (e: Exception) {
+                "error in getting public ip"
+            }
+        }
+
+        @SuppressLint("SetTextI18n")
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            ipText?.post(Runnable { ipText?.text  = "Public IP: $result" } )
+        }
     }
 }
