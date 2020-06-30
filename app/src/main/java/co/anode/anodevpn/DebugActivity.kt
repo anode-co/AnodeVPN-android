bb418bd6cf2bd66df518710d64f209ed2faf6d20
dkoukoul <dimitris.koukoulakis@gmail.com>
2020-06-30 10:57:30 +0300

    upgrade debug screen

--- app/src/main/java/co/anode/anodevpn/DebugActivity.kt
+++ app/src/main/java/co/anode/anodevpn/DebugActivity.kt
@@ -5,47 +5,68 @@ import android.content.Context
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
 import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.text.HtmlCompat
 import java.net.URL
 
 class DebugActivity : AppCompatActivity() {
+
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_debug)
 
-        val anodeUtil = AnodeUtil(application)
+        val buttonSubmitLogs = findViewById<Button>(R.id.button_SubmitLogs)
+        buttonSubmitLogs.setOnClickListener {
+            Toast.makeText(baseContext, "Sending log files to Anode server...", Toast.LENGTH_LONG).show()
+            AnodeClient.mycontext = baseContext
+            AnodeClient.PostLogs().execute()
+        }
+
         Thread(Runnable {
             Log.i(LOGTAG, "DebugActivity.RefreshValues startup")
+            val anodeUtil = AnodeUtil(application)
             while (true) {
-                val appversion = findViewById<TextView>(R.id.text_AppVersion)
-                appversion.text = "App Version: "+BuildConfig.VERSION_NAME
-                val ipv4 = findViewById<TextView>(R.id.text_ipv4)
-                ipv4.text = "IPv4: "+CjdnsSocket.ipv4Address
-                val ipv6 = findViewById<TextView>(R.id.text_ipv6)
-                ipv6.text = "IPv6: "+CjdnsSocket.ipv6Address
-                val pubkey = findViewById<TextView>(R.id.text_pubkey)
-                pubkey.text = "Public Key: "+anodeUtil.getPubKey()
-                val username = findViewById<TextView>(R.id.text_username)
-                val prefs = getSharedPreferences("co.anode.AnodeVPN", Context.MODE_PRIVATE)
-                username.text = "Username: "+prefs.getString("username","")
-                val node = findViewById<TextView>(R.id.text_node)
-                node.text = "Last Node: "+prefs.getString("ServerPublicKey","")
-                val peerstats = findViewById<TextView>(R.id.text_peerstats)
-                val peers:ArrayList<Benc.Bdict> = CjdnsSocket.InterfaceController_peerStats()
-                var info = ""
-                for (i in 0 until peers.count()) {
-                    info += peers[i].toString()
-                }
-                peerstats.text = "Peer stats: $info"
-                val publicip = findViewById<TextView>(R.id.text_publicIP)
-                publicip.text = "Public IP: updating ip..."
-                GetPublicIP().execute(publicip)
-                Thread.sleep(4000)
+                this.runOnUiThread(Runnable {
+                    val appversion = findViewById<TextView>(R.id.text_AppVersion)
+                    appversion.text = "App Version: "+BuildConfig.VERSION_NAME
+                    val ipv4 = findViewById<TextView>(R.id.text_ipv4)
+                    ipv4.text = "IPv4: "+CjdnsSocket.ipv4Address
+                    val ipv6 = findViewById<TextView>(R.id.text_ipv6)
+                    ipv6.text = "IPv6: "+CjdnsSocket.ipv6Address
+                    val pubkey = findViewById<TextView>(R.id.text_pubkey)
+                    pubkey.text = "Public Key: "+anodeUtil.getPubKey()
+                    val nodeLink = findViewById<TextView>(R.id.text_nodes)
+                    val link: Spanned = HtmlCompat.fromHtml("<a href='http://h.snode.cjd.li/#'"+CjdnsSocket.ipv6Address+">Node map</a>", HtmlCompat.FROM_HTML_MODE_LEGACY)
+                    nodeLink.movementMethod = LinkMovementMethod.getInstance()
+                    nodeLink.text = link
+                    val username = findViewById<TextView>(R.id.text_username)
+                    val prefs = getSharedPreferences("co.anode.AnodeVPN", Context.MODE_PRIVATE)
+                    username.text = "Username: "+prefs.getString("username","")
+                    val node = findViewById<TextView>(R.id.text_node)
+                    node.text = "Last Node: "+prefs.getString("ServerPublicKey","")
+                    val peerstats = findViewById<TextView>(R.id.text_peerstats)
+                    val peers:ArrayList<Benc.Bdict> = CjdnsSocket.InterfaceController_peerStats()
+                    var info = ""
+                    for (i in 0 until peers.count()) {
+                        info += peers[i]["lladdr"].toString() + " " + peers[i]["addr"].toString() + " " + peers[i]["state"].toString() + " in " + peers[i]["recvKbps"].toString() +" out "+peers[i]["sendKbps"].toString() + " "+ peers[i]["user"].toString()+"\n"
+                    }
+                    peerstats.text = "Peer stats: $info"
+                    val publicip = findViewById<TextView>(R.id.text_publicIP)
+                    publicip.text = "Public IP: updating ip..."
+                    GetPublicIP().execute(publicip)
+                })
+                Thread.sleep(8000)
             }
         }, "DebugActivity.RefreshValues").start()
-
     }
 
     class GetPublicIP(): AsyncTask<TextView, Void, String>() {
