aba361c5caeb4fca5e17ecd1573d24728d7c4f3a
dkoukoul <dimitris.koukoulakis@gmail.com>
2020-12-16 19:09:34 +0200

    update UI on debug activity

--- app/src/main/java/co/anode/anodium/DebugActivity.kt
+++ app/src/main/java/co/anode/anodium/DebugActivity.kt
@@ -24,7 +24,7 @@ class DebugActivity : AppCompatActivity() {
         //actionbar
         val actionbar = supportActionBar
         //set actionbar title
-        actionbar!!.title = "Debug"
+        actionbar!!.title = "View Logs"
         //set back button
         actionbar.setDisplayHomeAsUpEnabled(true)
         val buttonSubmitLogs = findViewById<Button>(R.id.button_SubmitLogs)
@@ -33,6 +33,12 @@ class DebugActivity : AppCompatActivity() {
             AnodeClient.mycontext = baseContext
             AnodeClient.storeError(baseContext, "other", Throwable("User submitted logs"))
             AnodeClient.PostLogs()
+        }
+        val buttonDeleteAccount = findViewById<Button>(R.id.button_Deleteaccount)
+        buttonDeleteAccount.setOnClickListener {
+            Log.i(LOGTAG, "Delete account")
+            AnodeClient.eventLog(baseContext, "Delete account selected")
+            AnodeClient.DeleteAccount().execute()
         }
 
         Thread(Runnable {
@@ -43,30 +49,30 @@ class DebugActivity : AppCompatActivity() {
                     val appversion = findViewById<TextView>(R.id.text_AppVersion)
                     appversion.text = "App Version: " + BuildConfig.VERSION_NAME
                     val ipv4 = findViewById<TextView>(R.id.text_ipv4)
-                    ipv4.text = "IPv4: " + CjdnsSocket.ipv4Address
+                    ipv4.text = CjdnsSocket.ipv4Address
                     val cjdnsipv6 = findViewById<TextView>(R.id.text_cjdnsipv6)
                     val nodeinfo = CjdnsSocket.Core_nodeInfo()
-                    cjdnsipv6.text = "cjdns IPv6: " + nodeinfo["myIp6"].str()
+                    cjdnsipv6.text = nodeinfo["myIp6"].str()
                     val internetipv6 = findViewById<TextView>(R.id.text_internetipv6)
-                    internetipv6.text = "internet IPv6: " + CjdnsSocket.ipv6Address
+                    internetipv6.text = CjdnsSocket.ipv6Address
                     val pubkey = findViewById<TextView>(R.id.text_pubkey)
-                    pubkey.text = "Public Key: " + anodeUtil.getPubKey()
+                    pubkey.text = anodeUtil.getPubKey()
                     val nodeLink = findViewById<TextView>(R.id.text_nodes)
                     val link: Spanned = HtmlCompat.fromHtml("<a href='http://h.snode.cjd.li/#" + nodeinfo["myIp6"].str() + "'>Find yourself on the map</a>", HtmlCompat.FROM_HTML_MODE_LEGACY)
                     nodeLink.movementMethod = LinkMovementMethod.getInstance()
                     nodeLink.text = link
                     val username = findViewById<TextView>(R.id.text_username)
                     val prefs = getSharedPreferences("co.anode.anodium", Context.MODE_PRIVATE)
-                    username.text = "Username: " + prefs.getString("username", "")
+                    username.text = prefs.getString("username", "")
                     val node = findViewById<TextView>(R.id.text_node)
-                    node.text = "VPN exit key: " + prefs.getString("ServerPublicKey", "")
+                    node.text = prefs.getString("ServerPublicKey", "")
                     val peerstats = findViewById<TextView>(R.id.text_peerstats)
                     val peers: ArrayList<Benc.Bdict> = CjdnsSocket.InterfaceController_peerStats()
                     var info = ""
                     for (i in 0 until peers.count()) {
                         info += peers[i]["lladdr"].toString() + " " + peers[i]["addr"].toString() + " " + peers[i]["state"].toString() + " in " + peers[i]["recvKbps"].toString() + " out " + peers[i]["sendKbps"].toString() + " " + peers[i]["user"].toString() + "\n"
                     }
-                    peerstats.text = "Peer stats: $info"
+                    peerstats.text = info
 
                     val listconnnections = findViewById<TextView>(R.id.text_listconnections)
                     val connections:ArrayList<Benc.Bdict> = CjdnsSocket.IpTunnel_listConnections()
@@ -74,9 +80,9 @@ class DebugActivity : AppCompatActivity() {
                     for (i in 0 until connections.count()) {
                          info += connections[i].toString()
                     }
-                    listconnnections.text = "Connections: $info"
+                    listconnnections.text = info
                     val publicip = findViewById<TextView>(R.id.text_publicIP)
-                    publicip.text = "Public IP: updating ip..."
+                    publicip.text = "updating ip..."
                     GetPublicIP().execute(publicip)
                 })
                 Thread.sleep(8000)
@@ -103,7 +109,7 @@ class DebugActivity : AppCompatActivity() {
         @SuppressLint("SetTextI18n")
         override fun onPostExecute(result: String?) {
             super.onPostExecute(result)
-            ipText?.post(Runnable { ipText?.text = "Public IP: $result" })
+            ipText?.post(Runnable { ipText?.text = result })
         }
     }
 }
