fd5a99ba0e274dc7f0f5f5cb018c57d2ae8b3d5f
dkoukoul <dimitris.koukoulakis@gmail.com>
2021-06-03 16:20:25 +0300

    transaction list scrollable

--- app/src/main/java/co/anode/anodium/WalletFragmentMain.kt
+++ app/src/main/java/co/anode/anodium/WalletFragmentMain.kt
@@ -57,8 +57,12 @@ class WalletFragmentMain : Fragment() {
             AnodeClient.eventLog(requireContext(), "Button: Copy wallet address clicked")
             Toast.makeText(context, "address has been copied", Toast.LENGTH_LONG)
         }
+        val history = v.findViewById<TextView>(R.id.texthistory)
 
-        val parentlayout = v.findViewById<ConstraintLayout>(R.id.wallet_fragmentMain)
+        history.setOnClickListener {
+            AnodeClient.eventLog(requireContext(), "Button: Older transactions clicked")
+            //TODO: open new activity
+        }
 
         val walletBalance = v.findViewById<TextView>(R.id.walletBalanceNumber)
         val listsLayout = v.findViewById<LinearLayout>(R.id.paymentsList)
@@ -69,179 +73,156 @@ class WalletFragmentMain : Fragment() {
 
         if (transactions.count() > prevtransactions.count()) {
             prevtransactions = transactions
-            var tcount = transactions.count()
-            if (tcount > 25) {
-                tcount = 25
-            }
-            for (i in 0 until tcount) {
-                //Add new line
-                var line = ConstraintLayout(context)
-                line.id = i
-                //line.orientation = LinearLayout.HORIZONTAL
-                val llparams =
-                    ConstraintLayout.LayoutParams(
-                        ConstraintLayout.LayoutParams.MATCH_PARENT,
-                        ConstraintLayout.LayoutParams.WRAP_CONTENT
-                    )
-                line.layoutParams = llparams
-                line.setPadding(10, 10, 10, 10)
 
-                //ADDRESS
-                val textaddress = TextView(context)
-                textaddress.id = View.generateViewId()
-                textaddress.text = transactions[i].destAddressesList[0].substring(4,20) +"..."
-                line.addView(textaddress)
-                //In/Out Icon
-                val icon = ImageView(context)
-                icon.id = View.generateViewId()
-                val amount:Float = transactions[i].amount.toFloat()/ 1073741824
-                if (amount < 0) {
-                    icon.setBackgroundResource(R.drawable.ic_baseline_arrow_upward_24)
-                } else {
-                    icon.setBackgroundResource(R.drawable.ic_baseline_arrow_downward_24)
-                }
-                line.addView(icon)
-                //AMOUNT
-                val textamount = TextView(context)
-                textamount.id = View.generateViewId()
-                textamount.text = "PKT%.2f".format(amount)
-                line.addView(textamount)
-                //DATE
-                val textDate = TextView(context)
-                textDate.id = View.generateViewId()
-                textDate.text = simpleDate.format(Date(transactions[i].timeStamp * 1000))
-                line.addView(textDate)
-
-                val set = ConstraintSet()
-                set.clear(textaddress.id)
-                set.clear(icon.id)
-                set.clear(textamount.id)
-                set.clear(textDate.id)
-                //address with start
-                set.connect(textaddress.id, ConstraintSet.START, line.id, ConstraintSet.START, 10)
-                //date with end
-                set.connect(textDate.id, ConstraintSet.END, line.id, ConstraintSet.END, 10)
-                //address with icon
-                set.connect(icon.id, ConstraintSet.START, textaddress.id, ConstraintSet.END,0)
-                set.connect(textaddress.id, ConstraintSet.END, icon.id, ConstraintSet.START,0)
-                //icon with amount
-                set.connect(textamount.id, ConstraintSet.START, icon.id, ConstraintSet.END,0)
-                set.connect(icon.id, ConstraintSet.END, textamount.id, ConstraintSet.START,0)
-                //amount with date
-                set.connect(textDate.id, ConstraintSet.START, textamount.id, ConstraintSet.END,0)
-                set.connect(textamount.id, ConstraintSet.END, textDate.id, ConstraintSet.START,0)
-                val chainViews = intArrayOf(textaddress.id, textDate.id)
-                val chainWeights = floatArrayOf(0f,0f)
-                set.createHorizontalChain(
-                    ConstraintSet.PARENT_ID, ConstraintSet.RIGHT,
-                    ConstraintSet.PARENT_ID, ConstraintSet.LEFT,
-                    chainViews, chainWeights,
-                    ConstraintSet.CHAIN_SPREAD)
-                set.constrainWidth(textaddress.id, ConstraintSet.WRAP_CONTENT)
-                set.constrainWidth(icon.id, ConstraintSet.WRAP_CONTENT)
-                set.constrainWidth(textamount.id, ConstraintSet.WRAP_CONTENT)
-                set.constrainWidth(textDate.id, ConstraintSet.WRAP_CONTENT)
-                set.constrainHeight(textaddress.id, ConstraintSet.WRAP_CONTENT)
-                set.constrainHeight(icon.id, ConstraintSet.WRAP_CONTENT)
-                set.constrainHeight(textamount.id, ConstraintSet.WRAP_CONTENT)
-                set.constrainHeight(textDate.id, ConstraintSet.WRAP_CONTENT)
-                set.applyTo(line)
-                listsLayout.addView(line)
-            }
         }//transactions
 
+        //Updating main wallet screen every 10 secs
         Thread(Runnable {
             var prevtransactions : MutableList<Transaction> = ArrayList()
             while(true) {
                 var transactions = LndRPCController.getTransactions()
                 if (transactions.count() > prevtransactions.count()) {
                     prevtransactions = transactions
-
-                    //for (i in 0 until transactions.count()) {
-
+                    var tcount = transactions.count()
+                    if (tcount > 25) {
+                        tcount = 25
+                    }
                     activity?.runOnUiThread {
                         //Set balance
                         walletBalance.text = "%.2f".format(LndRPCController.getTotalBalance())
-                        /*
-                        val simpleDate = SimpleDateFormat("dd/MM/yyyy")
+                        for (x in 0 until 10) {
+                            for (i in 0 until tcount) {
+                                //Add new line
+                                var line = ConstraintLayout(context)
+                                line.id = i
+                                //line.orientation = LinearLayout.HORIZONTAL
+                                val llparams =
+                                    ConstraintLayout.LayoutParams(
+                                        ConstraintLayout.LayoutParams.MATCH_PARENT,
+                                        ConstraintLayout.LayoutParams.WRAP_CONTENT
+                                    )
+                                line.layoutParams = llparams
+                                line.setPadding(10, 20, 10, 20)
 
-                        if (transactions.count() > 0) {
-                            val layout = v.findViewById<LinearLayout>(R.id.PaymentRowFirstLayout)
-                            layout.visibility = View.VISIBLE
-                            firstaddress.text = transactions[0].destAddressesList[0]
-                            if (transactions[0].amount < 0) {
-                                val icon = v.findViewById<ImageView>(R.id.firstpaymenticon)
-                                icon.setImageResource(R.drawable.ic_baseline_arrow_upward_24)
-                                DrawableCompat.setTint(
-                                    icon.drawable,
-                                    ContextCompat.getColor(
-                                        requireContext(),
-                                        android.R.color.holo_red_light
-                                    )
-                                )
-                            }
-                            firstamount.text =
-                                getString(R.string.wallet_coin) + (transactions[0].amount / 1073741824).toString()
+                                //ADDRESS
+                                val textaddress = TextView(context)
+                                textaddress.id = View.generateViewId()
+                                textaddress.width = 550
+                                textaddress.text =
+                                    transactions[i].destAddressesList[0].substring(4, 20) + "..."
+                                line.addView(textaddress)
+                                //In/Out Icon
+                                val icon = ImageView(context)
+                                icon.id = View.generateViewId()
+                                val amount: Float = transactions[i].amount.toFloat() / 1073741824
+                                if (amount < 0) {
+                                    icon.setBackgroundResource(R.drawable.ic_baseline_arrow_upward_24)
+                                } else {
+                                    icon.setBackgroundResource(R.drawable.ic_baseline_arrow_downward_24)
+                                }
+                                line.addView(icon)
+                                //AMOUNT
+                                val textamount = TextView(context)
+                                textamount.id = View.generateViewId()
+                                textamount.width = 300
+                                textamount.text = "PKT%.2f".format(amount)
+                                line.addView(textamount)
+                                //DATE
+                                val textDate = TextView(context)
+                                textDate.id = View.generateViewId()
+                                textDate.text =
+                                    simpleDate.format(Date(transactions[i].timeStamp * 1000))
+                                line.addView(textDate)
 
-                            firstdate.text = simpleDate.format(Date(transactions[0].timeStamp))
-                        }
-                        if (transactions.count() > 1) {
-                            val layout = v.findViewById<LinearLayout>(R.id.PaymentRowSecondLayout)
-                            layout.visibility = View.VISIBLE
-                            secondaddress.text = transactions[1].destAddressesList[0]
-                            secondamount.text =
-                                getString(R.string.wallet_coin) + (transactions[1].amount / 1073741824).toString()
-                            seconddate.text = simpleDate.format(Date(transactions[1].timeStamp))
-                            if (transactions[1].amount < 0) {
-                                val icon = v.findViewById<ImageView>(R.id.Secondpaymenticon)
-                                icon.setImageResource(R.drawable.ic_baseline_arrow_upward_24)
-                                DrawableCompat.setTint(
-                                    icon.drawable,
-                                    ContextCompat.getColor(
-                                        requireContext(),
-                                        android.R.color.holo_red_light
-                                    )
+                                val set = ConstraintSet()
+                                set.clear(textaddress.id)
+                                set.clear(icon.id)
+                                set.clear(textamount.id)
+                                set.clear(textDate.id)
+                                //address with start
+                                set.connect(
+                                    textaddress.id,
+                                    ConstraintSet.START,
+                                    line.id,
+                                    ConstraintSet.START,
+                                    10
                                 )
+                                //date with end
+                                set.connect(
+                                    textDate.id,
+                                    ConstraintSet.END,
+                                    line.id,
+                                    ConstraintSet.END,
+                                    10
+                                )
+                                //address with icon
+                                set.connect(
+                                    icon.id,
+                                    ConstraintSet.START,
+                                    textaddress.id,
+                                    ConstraintSet.END,
+                                    0
+                                )
+                                set.connect(
+                                    textaddress.id,
+                                    ConstraintSet.END,
+                                    icon.id,
+                                    ConstraintSet.START,
+                                    0
+                                )
+                                //icon with amount
+                                set.connect(
+                                    textamount.id,
+                                    ConstraintSet.START,
+                                    icon.id,
+                                    ConstraintSet.END,
+                                    0
+                                )
+                                set.connect(
+                                    icon.id,
+                                    ConstraintSet.END,
+                                    textamount.id,
+                                    ConstraintSet.START,
+                                    0
+                                )
+                                //amount with date
+                                set.connect(
+                                    textDate.id,
+                                    ConstraintSet.START,
+                                    textamount.id,
+                                    ConstraintSet.END,
+                                    0
+                                )
+                                set.connect(
+                                    textamount.id,
+                                    ConstraintSet.END,
+                                    textDate.id,
+                                    ConstraintSet.START,
+                                    0
+                                )
+                                val chainViews = intArrayOf(textaddress.id, textDate.id)
+                                val chainWeights = floatArrayOf(0f, 0f)
+                                set.createHorizontalChain(
+                                    ConstraintSet.PARENT_ID, ConstraintSet.RIGHT,
+                                    ConstraintSet.PARENT_ID, ConstraintSet.LEFT,
+                                    chainViews, chainWeights,
+                                    ConstraintSet.CHAIN_SPREAD
+                                )
+                                set.constrainWidth(textaddress.id, ConstraintSet.WRAP_CONTENT)
+                                set.constrainWidth(icon.id, ConstraintSet.WRAP_CONTENT)
+                                set.constrainWidth(textamount.id, ConstraintSet.WRAP_CONTENT)
+                                set.constrainWidth(textDate.id, ConstraintSet.WRAP_CONTENT)
+                                set.constrainHeight(textaddress.id, ConstraintSet.WRAP_CONTENT)
+                                set.constrainHeight(icon.id, ConstraintSet.WRAP_CONTENT)
+                                set.constrainHeight(textamount.id, ConstraintSet.WRAP_CONTENT)
+                                set.constrainHeight(textDate.id, ConstraintSet.WRAP_CONTENT)
+                                set.applyTo(line)
+                                listsLayout.addView(line)
                             }
                         }
-                        if (transactions.count() > 2) {
-                            val layout = v.findViewById<LinearLayout>(R.id.PaymentRowThirdLayout)
-                            layout.visibility = View.VISIBLE
-                            thirdaddress.text = transactions[2].destAddressesList[0]
-                            thirdamount.text =
-                                getString(R.string.wallet_coin) + (transactions[2].amount / 1073741824).toString()
-                            thirdddate.text = simpleDate.format(Date(transactions[2].timeStamp))
-                            if (transactions[2].amount < 0) {
-                                val icon = v.findViewById<ImageView>(R.id.Thirdpaymenticon)
-                                icon.setImageResource(R.drawable.ic_baseline_arrow_upward_24)
-                                DrawableCompat.setTint(
-                                    icon.drawable,
-                                    ContextCompat.getColor(
-                                        requireContext(),
-                                        android.R.color.holo_red_light
-                                    )
-                                )
-                            }
+                        if (transactions.count() > 25) {
+                            history.visibility = View.VISIBLE
                         }
-                        if (transactions.count() > 3) {
-                            val layout = v.findViewById<LinearLayout>(R.id.PaymentRowFourthLayout)
-                            layout.visibility = View.VISIBLE
-                            fourthaddress.text = transactions[3].destAddressesList[0]
-                            fourthamount.text =
-                                getString(R.string.wallet_coin) + (transactions[3].amount / 1073741824).toString()
-                            fourthdate.text = simpleDate.format(Date(transactions[3].timeStamp))
-                            if (transactions[3].amount < 0) {
-                                val icon = v.findViewById<ImageView>(R.id.Fourthpaymenticon)
-                                icon.setImageResource(R.drawable.ic_baseline_arrow_upward_24)
-                                DrawableCompat.setTint(
-                                    icon.drawable,
-                                    ContextCompat.getColor(
-                                        requireContext(),
-                                        android.R.color.holo_red_light
-                                    )
-                                )
-                            }
-                        }*/
                     }
                 }
                 Thread.sleep(10000)
