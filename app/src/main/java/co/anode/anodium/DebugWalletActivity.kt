6d570608ba2f9eef0cb1514e53944c483d17989d
dkoukoul <dimitris.koukoulakis@gmail.com>
2021-06-17 11:26:29 +0300

    improve scrolling on wallet debug

--- app/src/main/java/co/anode/anodium/DebugWalletActivity.kt
+++ app/src/main/java/co/anode/anodium/DebugWalletActivity.kt
@@ -3,6 +3,7 @@ package co.anode.anodium
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.ScrollView
 import android.widget.TextView
 import androidx.core.view.children
@@ -10,6 +11,9 @@ import java.io.File
 
 class DebugWalletActivity : AppCompatActivity() {
     private var scrollposition = 0
+    private var toBottom = false
+    private var logfile = ""
+    private var autoscroll = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,34 +27,42 @@ class DebugWalletActivity : AppCompatActivity() {
         //Open pltd log file and display it
         val logtext = findViewById<TextView>(R.id.debugwalletlogtext)
         val anodeUtil = AnodeUtil(application)
+        logfile = File(anodeUtil.CJDNS_PATH+"/"+anodeUtil.PLTD_LOG).readText()
+        logtext.text = logfile
         val scroll = findViewById<ScrollView>(R.id.wallet_debug_scroll)
+        scroll.post {
+            scroll.fullScroll(View.FOCUS_DOWN)
+        }
+
         scroll.setOnScrollChangeListener { v, scrollX, scrollY, oldScrollX, oldScrollY ->
             scrollposition = scrollY
+            val diff = scroll.getChildAt(0).bottom - (scrollY + scroll.height)
+            toBottom = diff < 50
         }
 
         Thread(Runnable {
             Log.i(LOGTAG, "DebugActivity.RefreshValues startup")
             var sleep: Long = 500
-            val anodeUtil = AnodeUtil(application)
-            var logfile = File(anodeUtil.CJDNS_PATH+"/"+anodeUtil.PLTD_LOG).readText()
-            this.runOnUiThread(Runnable {
-                logtext.text = logfile
-            })
             var oldlog = logfile
             while (true) {
                 this.runOnUiThread(Runnable {
-                    var newlog = File(anodeUtil.CJDNS_PATH+"/"+anodeUtil.PLTD_LOG).readText()
-                    if (newlog.length > oldlog.length) {
-                        oldlog = newlog
-                        logtext.text = newlog
-                        if (scrollposition + 6 > scroll.getChildAt(0).height) {
-                            scroll.scrollTo(0,scroll.getChildAt(0).height)
+                    if (toBottom) {
+                        var newlog =
+                            File(anodeUtil.CJDNS_PATH + "/" + anodeUtil.PLTD_LOG).readText()
+                        if (newlog.length > oldlog.length) {
+                            oldlog = newlog
+                            logtext.text = newlog
+                            //position is in characters?
+                            //scroll.fullScroll(View.FOCUS_DOWN)
+                            scroll.post {
+                                scroll.fullScroll(View.FOCUS_DOWN)
+                            }
+                            sleep = 1000
                         } else {
-                            scroll.scrollTo(0,scrollposition)
+                            sleep = 300
                         }
-                        sleep = 2000
                     } else {
-                        sleep = 500
+                        sleep = 1000
                     }
                 })
                 Thread.sleep(sleep)
--- app/src/main/java/co/anode/anodium/WalletStatsActivity.kt
+++ app/src/main/java/co/anode/anodium/WalletStatsActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import java.io.File
 import java.text.SimpleDateFormat
 import java.util.*
@@ -26,26 +27,28 @@ class WalletStatsActivity : AppCompatActivity() {
             val debugWalletActivity = Intent(this, DebugWalletActivity::class.java)
             startActivity(debugWalletActivity)
         }
+        val prefs = getSharedPreferences("co.anode.anodium", MODE_PRIVATE)
         val walletfile = File("$filesDir/lnd/data/chain/pkt/mainnet/wallet.db")
-        if (!walletfile.exists()) {
-            return
-        }
-        val myaddress = findViewById<TextView>(R.id.wstats_address)
-        val prefs = getSharedPreferences("co.anode.anodium", Context.MODE_PRIVATE)
-        myaddress.text = prefs.getString("lndwalletaddress", "")
-        val mybalance = findViewById<TextView>(R.id.wstats_balance)
-        mybalance.text = "PKT %.2f".format(LndRPCController.getTotalBalance())
-        val response = LndRPCController.getInfo()
-        if (response != null) {
-            val blockHeight = findViewById<TextView>(R.id.wstats_bheight)
-            blockHeight.text = response.blockHeight.toString()
-            val blockhash = findViewById<TextView>(R.id.wstats_bhash)
-            blockhash.text = response.blockHash
-            val headertimestamp = findViewById<TextView>(R.id.wstats_bheader)
-            val simpleDate = SimpleDateFormat("dd/MM/yyyy")
-            headertimestamp.text = simpleDate.format(Date(response.bestHeaderTimestamp * 1000))
-            val synced = findViewById<TextView>(R.id.wstats_synced)
-            synced.text = response.syncedToChain.toString()
+        if (walletfile.exists() && !prefs.getBoolean("lndwalletopened", false)) {
+            Toast.makeText(baseContext, "PKT wallet is not unlocked please try again.", Toast.LENGTH_LONG).show()
+        } else {
+            val myaddress = findViewById<TextView>(R.id.wstats_address)
+            val prefs = getSharedPreferences("co.anode.anodium", Context.MODE_PRIVATE)
+            myaddress.text = prefs.getString("lndwalletaddress", "")
+            val mybalance = findViewById<TextView>(R.id.wstats_balance)
+            mybalance.text = "PKT %.2f".format(LndRPCController.getTotalBalance())
+            val response = LndRPCController.getInfo()
+            if (response != null) {
+                val blockHeight = findViewById<TextView>(R.id.wstats_bheight)
+                blockHeight.text = response.blockHeight.toString()
+                val blockhash = findViewById<TextView>(R.id.wstats_bhash)
+                blockhash.text = response.blockHash
+                val headertimestamp = findViewById<TextView>(R.id.wstats_bheader)
+                val simpleDate = SimpleDateFormat("dd/MM/yyyy")
+                headertimestamp.text = simpleDate.format(Date(response.bestHeaderTimestamp * 1000))
+                val synced = findViewById<TextView>(R.id.wstats_synced)
+                synced.text = response.syncedToChain.toString()
+            }
         }
     }
 
