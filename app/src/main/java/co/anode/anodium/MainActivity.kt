b500325bc6ce999f1fefb7fb4fb77ae74afe2eeb
dkoukoul <dimitris.koukoulakis@gmail.com>
2021-07-01 13:34:32 +0300

    opening wallet on wallet fragment main added toasts when errors occur in posting errors

--- app/src/main/java/co/anode/anodium/MainActivity.kt
+++ app/src/main/java/co/anode/anodium/MainActivity.kt
@@ -336,31 +336,6 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }, "MainActivity.CheckUpdates").start()
-
-        AnodeClient.eventLog(baseContext, "Application launched")
-
-        //Open PKT Wallet
-        Thread(Runnable {
-            Thread.sleep(2000)
-            val walletfile = File(baseContext.filesDir.toString() + "/lnd/data/chain/pkt/mainnet/wallet.db")
-            while(!prefs.getBoolean("lndwalletopened", false)) {
-                if (!walletfile.exists()) {
-                    Thread.sleep(2000)
-                } else {
-                    openPKTWallet()
-                    Thread.sleep(1000)
-                    if (prefs.getBoolean("lndwalletopened", false)) {
-                        runOnUiThread {
-                            Toast.makeText(
-                                applicationContext,
-                                "PKT Wallet unlocked",
-                                Toast.LENGTH_LONG
-                            ).show()
-                        }
-                    }
-                }
-            }
-        }, "MainActivity.OpenPKTWallet").start()
     }
 
     fun bigbuttonState(state: Int) {
@@ -449,12 +424,17 @@ class MainActivity : AppCompatActivity() {
             }
             //Close app
             finishAffinity()
-            System.exit(0)
+            exitProcess(0)
         }
         setUsernameTopBar()
-        //Show/Hide Registration on menu
+
         if (mainMenu != null) {
-            mainMenu!!.findItem(R.id.action_account_settings).setVisible(!prefs.getBoolean("Registered", false))
+            //Show/Hide Registration on menu
+            mainMenu!!.findItem(R.id.action_account_settings).isVisible =
+                !prefs.getBoolean("Registered", false)
+            //Show/Hide Wallet Stats
+            mainMenu!!.findItem(R.id.action_wallet_stats).isVisible =
+                prefs.getBoolean("lndwalletopened", false)
         }
         //Set button to correct status
         val status = findViewById<TextView>(R.id.textview_status)
@@ -467,6 +447,12 @@ class MainActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
         uiInForeground = true
+        if (mainMenu != null) {
+            val prefs = getSharedPreferences("co.anode.anodium", MODE_PRIVATE)
+            mainMenu!!.findItem(R.id.action_wallet_stats).isVisible =
+                prefs.getBoolean("lndwalletopened", false)
+        }
+        AnodeClient.eventLog(baseContext, "Application launched")
     }
 
     override fun onPause() {
@@ -541,13 +527,8 @@ class MainActivity : AppCompatActivity() {
             return true
         } else if (id == R.id.action_wallet) {
             Log.i(LOGTAG, "Open wallet activity")
-            val walletfile = File(baseContext.filesDir.toString() + "/lnd/data/chain/pkt/mainnet/wallet.db")
-            if (walletfile.exists() && !prefs.getBoolean("lndwalletopened", false)){
-                Toast.makeText(baseContext, "PKT wallet is not unlocked please try again later.", Toast.LENGTH_LONG).show()
-            } else {
-                val walletactivity = Intent(applicationContext, WalletActivity::class.java)
-                startActivity(walletactivity)
-            }
+            val walletactivity = Intent(applicationContext, WalletActivity::class.java)
+            startActivity(walletactivity)
             return true
         } else if (id == R.id.action_wallet_stats) {
             Log.i(LOGTAG, "Open wallet stats activity")
@@ -706,95 +687,6 @@ class MainActivity : AppCompatActivity() {
         } catch (e: Exception) {
             return "None"
         }
-    }
-
-    fun openPKTWallet(): Boolean {
-        val prefs = applicationContext.getSharedPreferences("co.anode.anodium", AppCompatActivity.MODE_PRIVATE)
-        Log.i(LOGTAG, "MainActivity trying to open wallet")
-        var result = LndRPCController.openWallet(prefs)
-        if (result.contains("ErrWrongPassphrase")) {
-            var password = ""
-            val builder: AlertDialog.Builder = AlertDialog.Builder(applicationContext)
-            builder.setTitle("PKT Wallet")
-            builder.setMessage("Please type your PKT Wallet password")
-            val input = EditText(applicationContext)
-            val lp = LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.MATCH_PARENT
-            )
-            input.layoutParams = lp
-            builder.setView(input)
-            builder.setPositiveButton(
-                "Submit",
-                DialogInterface.OnClickListener { dialog, which ->
-                    password = input.text.toString()
-                    dialog.dismiss()
-
-                    if ((prefs != null) && (!password.isNullOrEmpty())) {
-                        with(prefs.edit()) {
-                            putString("walletpassword", password)
-                            commit()
-                        }
-                        val result = LndRPCController.openWallet(prefs)
-                        if (result == "OK") {
-                            with(prefs.edit()) {
-                                putBoolean("lndwalletopened", true)
-                                commit()
-                            }
-                            Toast.makeText(applicationContext, "PKT wallet is open", Toast.LENGTH_LONG)
-                                .show()
-                        } else {
-                            with(prefs.edit()) {
-                                putBoolean("lndwalletopened", false)
-                                commit()
-                            }
-                            Toast.makeText(applicationContext, "Wrong password.", Toast.LENGTH_LONG)
-                                .show()
-                        }
-                    }
-                })
-
-            builder.setNegativeButton(
-                "Cancel",
-                DialogInterface.OnClickListener { dialog, which ->
-                    dialog.dismiss()
-                })
-            val alert: androidx.appcompat.app.AlertDialog = builder.create()
-            alert.show()
-        } else if (result != "OK") {
-            //can not open wallet
-            Log.w(LOGTAG, "Can not open PKT wallet")
-            //wrong password prompt user to type password again
-            val datadir =
-                File(applicationContext.filesDir.toString() + "/lnd/data/chain/pkt/mainnet")
-            var checkwallet = result
-            if (!datadir.exists()) {
-                Log.e(LOGTAG, "expected folder structure not available")
-                checkwallet += " datadir does not exist "
-            } else {
-                checkwallet += " wallet.db exists "
-            }
-            if (prefs.getString("walletpassword", "").isNullOrEmpty()) {
-                Log.e(LOGTAG, "walletpassword in shared preferences is empty")
-                checkwallet += " walletpassword is empty"
-            } else {
-                checkwallet += " walletpassword is not empty"
-            }
-            /*AnodeClient.storeError(requireContext(), "other", Throwable(checkwallet))
-            val pltdlog =
-                File(anodeUtil!!.CJDNS_PATH + "/" + anodeUtil!!.PLTD_LOG).readText()
-            AnodeClient.storeError(baseContext, "other", Throwable(pltdlog))
-            Toast.makeText(baseContext, "Error in opening PKT wallet", Toast.LENGTH_LONG)
-                .show()*/
-            return false
-        } else if (result == "OK") {
-            with(prefs.edit()) {
-                putBoolean("lndwalletopened", true)
-                commit()
-            }
-            return true
-        }
-        return false
     }
 
     fun closeApp() {
