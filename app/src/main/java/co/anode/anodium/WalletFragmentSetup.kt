ceddc7b9b81316f3eebfa1328d5a41289658e096
dkoukoul <dimitris.koukoulakis@gmail.com>
2021-11-01 15:50:30 +0200

    fix issue #17

--- app/src/main/java/co/anode/anodium/WalletFragmentSetup.kt
+++ app/src/main/java/co/anode/anodium/WalletFragmentSetup.kt
@@ -11,10 +11,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 
 
 class WalletFragmentSetup : Fragment() {
+    lateinit var statusbar: TextView
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -30,14 +32,15 @@ class WalletFragmentSetup : Fragment() {
         AnodeClient.eventLog(requireContext(),"Activity: WalletFragmentCreate created")
         val prefs = context?.getSharedPreferences("co.anode.anodium", Context.MODE_PRIVATE)
         var seed = ""
-        val closebutton = view.findViewById<Button>(R.id.button_wallet_close)
-        closebutton.visibility = View.GONE
+        val closeButton = view.findViewById<Button>(R.id.button_wallet_close)
+        closeButton.visibility = View.GONE
         val textview = view.findViewById<TextView>(R.id.text_walletcreate_seed_instructions)
         textview.visibility = View.GONE
-        val seedlayout = view.findViewById<LinearLayout>(R.id.seed_layout)
-        seedlayout.visibility = View.GONE
-        val createbutton = view.findViewById<Button>(R.id.button_wallet_create)
-        createbutton.setOnClickListener {
+        val seedLayout = view.findViewById<LinearLayout>(R.id.seed_layout)
+        seedLayout.visibility = View.GONE
+        val createButton = view.findViewById<Button>(R.id.button_wallet_create)
+        statusbar = view.findViewById<TextView>(R.id.textview_status)
+        createButton.setOnClickListener {
             AnodeClient.eventLog(requireContext(),"Button: Create PKT wallet clicked")
             Log.i(LOGTAG, "WalletFragmentSetup creating wallet")
             var password = ""
@@ -57,35 +60,52 @@ class WalletFragmentSetup : Fragment() {
                 dialog.dismiss()
 
                 if ((prefs != null) && (password.isNotEmpty())) {
-                    val result = LndRPCController.createLocalWallet(prefs, password)
-                    if (result.contains("Success")) {
-                        Toast.makeText(requireContext(), "PKT wallet created", Toast.LENGTH_LONG).show()
-                        Log.i(LOGTAG, "WalletFragmentSetup retrieved seed phrase")
-                        seed = result.substring("Success".length)
-                        //Change label
-                        val label = view.findViewById<TextView>(R.id.text_walletcreate_label)
-                        label.text = context?.resources?.getString(R.string.wallet_create_seed_label)
-                        textview.visibility = View.VISIBLE
-                        val seedArray = seed.split(" ")
-                        val seedCol1 = view.findViewById<TextView>(R.id.seed_column1)
-                        val seedCol2 = view.findViewById<TextView>(R.id.seed_column2)
-                        var seed1 = ""
-                        var seed2 = ""
-                        for (i in 0 until 12) {
-                            seed1 += (i+1).toString()+". " + seedArray[i]+"\n"
+                    Thread(Runnable {
+                        activity?.runOnUiThread(Runnable {
+                                statusbar.text = "Creating wallet please wait..."
+                                val layout = view.findViewById<ConstraintLayout>(R.id.wallet_fragmentCreate)
+                                activity?.getColor(android.R.color.darker_gray)?.let { layout.setBackgroundColor(it) }
+                            })
+                        val result = LndRPCController.createLocalWallet(prefs, password)
+                        if (result.contains("Success")) {
+                            activity?.runOnUiThread(Runnable {
+                                Toast.makeText(
+                                    requireContext(),
+                                    "PKT wallet created",
+                                    Toast.LENGTH_LONG
+                                ).show()
+                                statusbar.text = ""
+                                val layout = view.findViewById<ConstraintLayout>(R.id.wallet_fragmentCreate)
+                                activity?.getColor(android.R.color.white)?.let { layout.setBackgroundColor(it) }
+                                Log.i(LOGTAG, "WalletFragmentSetup retrieved seed phrase")
+                                seed = result.substring("Success".length)
+                                //Change label
+                                val label = view.findViewById<TextView>(R.id.text_walletcreate_label)
+                                label.text = context?.resources?.getString(R.string.wallet_create_seed_label)
+                                textview.visibility = View.VISIBLE
+                                val seedArray = seed.split(" ")
+                                val seedCol1 = view.findViewById<TextView>(R.id.seed_column1)
+                                val seedCol2 = view.findViewById<TextView>(R.id.seed_column2)
+                                var seed1 = ""
+                                var seed2 = ""
+                                for (i in 0 until 12) {
+                                    seed1 += (i + 1).toString() + ". " + seedArray[i] + "\n"
+                                }
+                                for (i in 12 until seedArray.size - 1) {
+                                    seed2 += (i + 1).toString() + ". " + seedArray[i] + "\n"
+                                }
+                                seedCol1.text = seed1
+                                seedCol2.text = seed2
+                                seedLayout.visibility = View.VISIBLE
+                                createButton.visibility = View.GONE
+                                closeButton.visibility = View.VISIBLE
+                            })
+                        } else {
+                            Log.i(LOGTAG, "WalletFragmentSetup Error from trying to create wallet")
+                            Toast.makeText(requireContext(), "Error: $result", Toast.LENGTH_LONG).show()
                         }
-                        for (i in 12 until seedArray.size-1) {
-                            seed2 += (i+1).toString()+". " + seedArray[i]+"\n"
-                        }
-                        seedCol1.text = seed1
-                        seedCol2.text = seed2
-                        seedlayout.visibility = View.VISIBLE
-                        createbutton.visibility = View.GONE
-                        closebutton.visibility = View.VISIBLE
-                    } else {
-                        Log.i(LOGTAG, "WalletFragmentSetup Error from trying to create wallet")
-                        Toast.makeText(requireContext(), "Error: $result", Toast.LENGTH_LONG).show()
-                    }
+
+                    }, "WalletFragmentSetup.CreateWallet").start()
                 }
             })
 
@@ -96,7 +116,7 @@ class WalletFragmentSetup : Fragment() {
             alert.show()
         }
 
-        closebutton.setOnClickListener {
+        closeButton.setOnClickListener {
             activity?.finish()
         }
 
