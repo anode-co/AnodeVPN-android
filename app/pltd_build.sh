edb3aba53b132d7eb6b17ab2b510d10c6f89d215
dkoukoul <dimitris.koukoulakis@gmail.com>
2021-11-16 16:19:00 +0200

    add 32bit build

--- app/pltd_build.sh
+++ app/pltd_build.sh
@@ -1,16 +1,11 @@
 echo 'Building pltd emulator'
-export ANDROID_NDK_HOME=$HOME/Code/cjdns/build_android/android-ndk-r21
+export ANDROID_NDK_HOME=$HOME/Android/Ndk
 export PATH=$PATH:$HOME/go/bin
 cd ~/Code/pktd
 PKTD_PATH=$HOME/Code/pktd
-ANODIUM_ASSETS=$HOME/Code/AnodeVPN-android/app/src/main/assets
-GOMOBILE="$HOME/Applications/go/pkg/gomobile" GOOS=android GOARCH=386 CC=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/x86_64-linux-android21-clang CXX=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/x86_64-linux-android21-clang++ CGO_ENABLED=1 GOARM=7 go build -p=8 -pkgdir=$GOMOBILE/pkg_android_arm -tags="" -ldflags="-s -w -extldflags=-pie" -o $ANODIUM_ASSETS/x86_64/pltd -x $PKTD_PATH/lnd/cmd/lnd/main.go
-echo 'Compressing pltd emulator'
+export ANODIUM_LIBS=$HOME/Code/AnodeVPN-android/app/src/main/jniLibs
+GOMOBILE="$HOME/go/pkg/gomobile" GOOS=android GOARCH=386 CC=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/x86_64-linux-android29-clang CXX=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/x86_64-linux-android29-clang++ CGO_ENABLED=1 GOARM=7 go build -p=8 -pkgdir=$GOMOBILE/pkg_android_arm -tags="" -ldflags="-s -w -extldflags=-pie" -o $ANODIUM_LIBS/x86/libpltd.so -x $PKTD_PATH/lnd/cmd/lnd/main.go
 echo 'Building pltd aarch64'
-cd $ANODIUM_ASSETS/x86_64
-~/Applications/upx-3.96-amd64_linux/upx pltd
-cd ~/Code/pktd
-GOMOBILE="$HOME/Applications/go/pkg/gomobile" GOOS=android GOARCH=arm64 CC=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android21-clang CXX=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android28-clang++ CGO_ENABLED=1 GOARM=7 go build -p=8 -pkgdir=$GOMOBILE/pkg_android_arm -tags="" -ldflags="-s -w -extldflags=-pie" -o $ANODIUM_ASSETS/aarch64/pltd -x $PKTD_PATH/lnd/cmd/lnd/main.go
-echo 'Compressing pltd aarch64'
-cd $ANODIUM_ASSETS/aarch64
-~/Applications/upx-3.96-amd64_linux/upx pltd
+GOMOBILE="$HOME/go/pkg/gomobile" GOOS=android GOARCH=arm64 CC=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android29-clang CXX=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android29-clang++ CGO_ENABLED=1 GOARM=7 go build -p=8 -pkgdir=$GOMOBILE/pkg_android_arm -tags="" -ldflags="-s -w -extldflags=-pie" -o $ANODIUM_LIBS/arm64-v8a/libpltd.so -x $PKTD_PATH/lnd/cmd/lnd/main.go
+echo 'Building pltd armv7a'
+GOMOBILE="$HOME/go/pkg/gomobile" GOOS=android GOARCH=arm CC=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/armv7a-linux-androideabi29-clang CXX=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/armv7a-linux-androideabi29-clang++ CGO_ENABLED=1 GOARM=7 go build -p=8 -pkgdir=$GOMOBILE/pkg_android_arm -tags="" -ldflags="-s -w -extldflags=-pie" -o $ANODIUM_LIBS/armeabi-v7a/libpltd.so -x $PKTD_PATH/lnd/cmd/lnd/main.go
