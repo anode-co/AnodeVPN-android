217ab7b750012caf6fee6201244772fe9cb17b06
dkoukoul <dimitris.koukoulakis@gmail.com>
2022-02-28 15:25:19 +0200

    update transaction details

--- app/src/main/java/co/anode/anodium/TransactionDetailsFragment.kt
+++ app/src/main/java/co/anode/anodium/TransactionDetailsFragment.kt
@@ -1,18 +1,13 @@
 package co.anode.anodium
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.os.Bundle
-import android.text.Html
 import android.text.Spanned
 import android.text.method.LinkMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.RatingBar
 import android.widget.TextView
-import android.widget.ToggleButton
 import androidx.core.text.HtmlCompat
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
@@ -25,68 +20,37 @@ class TransactionDetailsFragment : BottomSheetDialogFragment(){
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val v = inflater.inflate(R.layout.fragment_transactiondetails, container, false)
-        val prefs = requireContext().getSharedPreferences("co.anode.anodium", Context.MODE_PRIVATE)
-        var myaddress = prefs.getString("lndwalletaddress", "")
-        var addresses = mutableListOf<String>()
-        var i=0
-        while(!requireArguments().getString("address$i").isNullOrEmpty()) {
-            requireArguments().getString("address$i")?.let { addresses.add(i, it) }
-            i += 1
+//        val prefs = requireContext().getSharedPreferences("co.anode.anodium", Context.MODE_PRIVATE)
+//        var myaddress = prefs.getString("lndwalletaddress", "")
+        var destAddress = ""
+        if (!requireArguments().getString("address").isNullOrEmpty()) {
+            destAddress = requireArguments().getString("address").toString()
         }
         val address = v.findViewById<TextView>(R.id.trdetails_addr)
-        address.text = ""
-        for (a in 0 until addresses.count()) {
-            val shortaddr = addresses[a].substring(0,9)+"..."+addresses[a].substring(addresses[a].length-8)
-            if (addresses[a] == myaddress) {
-                //color our address
-                address.append(Html.fromHtml("<font color='blue'>$shortaddr</font><br>"))
-            } else {
-                address.append(Html.fromHtml("<font color='black'>$shortaddr</font><br>"))
-            }
+        val paidToLabel = v.findViewById<TextView>(R.id.trdetails_addr_label)
+        if (destAddress.isNullOrEmpty()) {
+            paidToLabel.visibility = View.INVISIBLE
+        } else {
+            paidToLabel.visibility = View.VISIBLE
+            address.text = destAddress
         }
 
-        val textamount = v.findViewById<TextView>(R.id.trdetails_amount)
-        var amount = requireArguments().getLong("amount").toFloat()
-        val onePKT = 1073741824
-        val mPKT = 1073741.824F
-        val uPKT  = 1073.741824F
-        val nPKT  = 1.073741824F
-        if (amount > 1000000000) {
-            amount /= onePKT
-            textamount.text = "PKT %.2f".format(amount)
-        } else if (amount > 1000000) {
-            amount /= mPKT
-            textamount.text = "mPKT %.2f".format(amount)
-        } else if (amount > 1000) {
-            amount /= uPKT
-            textamount.text = "μPKT %.2f".format(amount)
-        } else if (amount < 1000000000) {
-            amount /= onePKT
-            textamount.text = "PKT %.2f".format(amount)
-        } else if (amount < 1000000) {
-            amount /= mPKT
-            textamount.text = "mPKT %.2f".format(amount)
-        } else if (amount < 1000) {
-            amount /= uPKT
-            textamount.text = "μPKT %.2f".format(amount)
-        } else {
-            amount /= nPKT
-            textamount.text = "nPKT %.2f".format(amount)
-        }
+        val textAmount = v.findViewById<TextView>(R.id.trdetails_amount)
+        textAmount.text = requireArguments().getString("amount")
 
         val block = v.findViewById<TextView>(R.id.trdetails_block)
         val bheight = requireArguments().getInt("blockheight")
         val bhash = requireArguments().getString("blockhash")
-        //val confirmations = v.findViewById<TextView>(R.id.trdetails_confirmations)
+        val confirmations = v.findViewById<TextView>(R.id.trdetails_confirmations)
         if (bheight>0) {
-            val blocklink: Spanned = HtmlCompat.fromHtml("<a href='https://explorer.pkt.cash/block/$bhash'>$bheight</a>", HtmlCompat.FROM_HTML_MODE_LEGACY)
+            val blockLink: Spanned = HtmlCompat.fromHtml("<a href='https://explorer.pkt.cash/block/$bhash'>$bheight</a>", HtmlCompat.FROM_HTML_MODE_LEGACY)
             block.movementMethod = LinkMovementMethod.getInstance()
-            block.text = blocklink
-            //confirmations.visibility = View.VISIBLE
-            //confirmations.text = "Confirmations: "+requireArguments().getInt("confirmations").toString()
+            block.text = blockLink
+            confirmations.visibility = View.VISIBLE
+            confirmations.text = "Confirmations: "+requireArguments().getInt("confirmations").toString()
         } else {
             block.text = "Unconfirmed"
-            //confirmations.visibility = View.INVISIBLE
+            confirmations.visibility = View.INVISIBLE
         }
         val txidlink = v.findViewById<TextView>(R.id.trdetails_txid)
         val txid = requireArguments().getString("txid")
