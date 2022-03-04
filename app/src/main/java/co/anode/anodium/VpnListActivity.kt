74ebfdf19f3c93b235189910e56d61a1d73019c9
dkoukoul <dimitris.koukoulakis@gmail.com>
2022-03-04 13:08:41 +0200

    move from asynctask to executor

--- app/src/main/java/co/anode/anodium/VpnListActivity.kt
+++ app/src/main/java/co/anode/anodium/VpnListActivity.kt
@@ -2,12 +2,11 @@
 
 package co.anode.anodium
 
-import android.os.AsyncTask
-import android.os.Bundle
-import android.util.Log
+import android.os.*
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import org.json.JSONArray
+import java.util.concurrent.Executors
 
 
 class VpnListActivity : AppCompatActivity() {
@@ -25,8 +24,6 @@ class VpnListActivity : AppCompatActivity() {
         val actionbar = supportActionBar
         //set back button
         actionbar?.setDisplayHomeAsUpEnabled(true)
-        //Retrieve Servers list
-        fetchVpnServers().execute()
         val searchView = findViewById<SearchView>(R.id.searchText)
 
         searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
@@ -42,58 +39,45 @@ class VpnListActivity : AppCompatActivity() {
         AnodeClient.eventLog("Activity: VPN List created")
     }
 
+    override fun onResume() {
+        super.onResume()
+        //Retrieve Servers list
+        val executor = Executors.newSingleThreadExecutor()
+        val handler = Handler(Looper.getMainLooper())
+        executor.execute {
+            val result = AnodeClient.APIHttpReq(apiServersList,"", "GET", needsAuth = true, isRetry = false)
+            handler.post {
+                if (result.isBlank()) {
+                    if (dataList.isNotEmpty()) findViewById<ListView>(R.id.listview_servers).adapter = VPNListAdapter(this@VpnListActivity, supportFragmentManager, dataList)
+                    return@post
+                }
+                val serversArr = JSONArray(result)
+                for (i in 0 until serversArr.length()) {
+                    val serverDetails = serversArr.getJSONObject(i)
+                    val map = HashMap<String, String>()
+                    map["publicKey"] = serverDetails.getString("publicKey")
+                    map["name"] = serverDetails.getString("name")
+                    map["countryCode"] = serverDetails.getString("countryCode")
+                    map["averageRating"] = serverDetails.getString("averageRating")
+                    map["cost"] = serverDetails.getString("cost")
+                    map["load"] = serverDetails.getInt("load").toString()
+                    map["quality"] = serverDetails.getInt("quality").toString()
+                    map["isFavorite"] = serverDetails.getString("isFavorite")
+                    map["onlineSinceDatetime"] = serverDetails.getString("onlineSinceDatetime")
+                    map["lastSeenDatetime"] = serverDetails.getString("lastSeenDatetime")
+                    map["numRatings"] = serverDetails.getInt("numRatings").toString()
+                    dataList.add(map)
+                }
+                //Sort list by country
+                dataList.sortWith(compareBy { it["countryCode"] })
+                adapter = VPNListAdapter(this@VpnListActivity,supportFragmentManager, dataList)
+                findViewById<ListView>(R.id.listview_servers).adapter = adapter
+            }
+        }
+    }
+
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
-    }
-
-    inner class fetchVpnServers : AsyncTask<String, Void, String>() {
-        override fun doInBackground(vararg params: String?): String? {
-            return try {
-                AnodeClient.statustv = findViewById(R.id.textview_status)
-                var url = apiServersList
-                if (params.isNotEmpty()) url = params[0].toString()
-                //return URL(url).readText(Charsets.UTF_8)
-                AnodeClient.APIHttpReq(url,"", "GET", needsAuth = true, isRetry = false)
-            } catch (e: Exception) {
-                Log.e(LOGTAG, "Error: "+e.message)
-                runOnUiThread {
-                    Toast.makeText(baseContext, "Error: "+e.message, Toast.LENGTH_LONG).show()
-                }
-                null
-            }
-        }
-
-        override fun onPostExecute(result: String?) {
-            super.onPostExecute(result)
-            if (result.isNullOrBlank()) {
-                if (dataList.isNotEmpty()) findViewById<ListView>(R.id.listview_servers).adapter = VPNListAdapter(this@VpnListActivity, supportFragmentManager, dataList)
-                return
-            }
-
-            val serversArr = JSONArray(result)
-            //val prefs = baseContext.getSharedPreferences("co.anode.anodium", Context.MODE_PRIVATE)
-
-            for (i in 0 until serversArr.length()) {
-                val serverDetails = serversArr.getJSONObject(i)
-                val map = HashMap<String, String>()
-                map["publicKey"] = serverDetails.getString("publicKey")
-                map["name"] = serverDetails.getString("name")
-                map["countryCode"] = serverDetails.getString("countryCode")
-                map["averageRating"] = serverDetails.getString("averageRating")
-                map["cost"] = serverDetails.getString("cost")
-                map["load"] = serverDetails.getInt("load").toString()
-                map["quality"] = serverDetails.getInt("quality").toString()
-                map["isFavorite"] = serverDetails.getString("isFavorite")
-                map["onlineSinceDatetime"] = serverDetails.getString("onlineSinceDatetime")
-                map["lastSeenDatetime"] = serverDetails.getString("lastSeenDatetime")
-                map["numRatings"] = serverDetails.getInt("numRatings").toString()
-                dataList.add(map)
-            }
-            //Sort list by country
-            dataList.sortWith(compareBy { it["countryCode"] })
-            adapter = VPNListAdapter(this@VpnListActivity,supportFragmentManager, dataList)
-            findViewById<ListView>(R.id.listview_servers).adapter = adapter
-        }
     }
 }
