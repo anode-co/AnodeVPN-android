605dfd36fdc704d5bf1fc9d461d4f93411a95788
dkoukoul <dimitris.koukoulakis@gmail.com>
2022-03-08 12:41:32 +0200

    removed asyncTask

--- app/src/main/java/co/anode/anodium/ChangePasswordActivity.kt
+++ app/src/main/java/co/anode/anodium/ChangePasswordActivity.kt
@@ -1,5 +1,3 @@
-@file:Suppress("DEPRECATION")
-
 package co.anode.anodium
 
 import android.content.Context
@@ -57,7 +55,7 @@ class ChangePasswordActivity : AppCompatActivity() {
                 newPassword.text.clear()
                 confirmPassword.text.clear()
             }else {
-                changePassword().execute(bForgotPassword.toString(), oldPassword.text.toString(), newPassword.text.toString())
+                changePassword(oldPassword.text.toString(), newPassword.text.toString(), bForgotPassword)
             }
         }
         AnodeClient.eventLog("Activity: Change password created")
@@ -68,42 +66,37 @@ class ChangePasswordActivity : AppCompatActivity() {
         return true
     }
 
-    inner class changePassword : AsyncTask<String, Void, String>() {
-        override fun doInBackground(vararg params: String?): String {
-            val prefs = getSharedPreferences("co.anode.anodium", Context.MODE_PRIVATE)
-            val jsonObject = JSONObject()
-            var url = apiChangePasswordUrl.replace("<username>", prefs.getString("username", "")!!, true)
-            if (params[0] == "false") {
-                jsonObject.accumulate("currentPassword", params[1])
-            } else {
-                //jsonObject.accumulate("passwordRecoveryToken", prefs.getString("passwordResetToken",""))
-                jsonObject.accumulate("passwordResetToken", prefs.getString("passwordResetToken", ""))
-                url = apiChangePasswordResetUrl.replace("<username>", prefs.getString("username", "")!!, true)
-            }
-            jsonObject.accumulate("newPassword", params[2])
-            val resp = AnodeClient.APIHttpReq(url, jsonObject.toString(), "POST", needsAuth = true, isRetry = false)
-            Log.i(LOGTAG, resp)
-            return resp
+    private fun changePassword(oldPassword:String, newPassword:String, bForgotPassword: Boolean): String {
+        val prefs = getSharedPreferences("co.anode.anodium", Context.MODE_PRIVATE)
+        val jsonObject = JSONObject()
+        var url = apiChangePasswordUrl.replace("<username>", prefs.getString("username", "")!!, true)
+        if (!bForgotPassword) {
+            jsonObject.accumulate("currentPassword", oldPassword)
+        } else {
+            jsonObject.accumulate("passwordResetToken", prefs.getString("passwordResetToken", ""))
+            url = apiChangePasswordResetUrl.replace("<username>", prefs.getString("username", "")!!, true)
         }
-
-        override fun onPostExecute(result: String?) {
-            super.onPostExecute(result)
-            Log.i(LOGTAG, "Received: $result")
-            if (result.isNullOrBlank()) {
-                //unknown
-                finish()
-            } else if (result.contains("ERROR: ")) {
-                Toast.makeText(baseContext, result, Toast.LENGTH_SHORT).show()
-            } else {
-                try {
-                    val jsonObj = JSONObject(result)
-                    if (jsonObj.has("status") && jsonObj.getString("status") == "success") {
-                        Toast.makeText(baseContext, "Password changed successfully", Toast.LENGTH_SHORT).show()
-                        thread.start()
-                    }
-                }catch (e: JSONException) {
-                    Toast.makeText(baseContext, result, Toast.LENGTH_SHORT).show()
+        jsonObject.accumulate("newPassword", newPassword)
+        val resp = AnodeClient.APIHttpReq(url, jsonObject.toString(), "POST", needsAuth = true, isRetry = false)
+        Log.i(LOGTAG, resp)
+        return resp
+    }
+    private fun changePasswordHandler(result:String) {
+        Log.i(LOGTAG, "Received: $result")
+        if (result.isBlank()) {
+            //unknown
+            finish()
+        } else if (result.contains("ERROR: ")) {
+            Toast.makeText(baseContext, result, Toast.LENGTH_SHORT).show()
+        } else {
+            try {
+                val jsonObj = JSONObject(result)
+                if (jsonObj.has("status") && jsonObj.getString("status") == "success") {
+                    Toast.makeText(baseContext, "Password changed successfully", Toast.LENGTH_SHORT).show()
+                    thread.start()
                 }
+            }catch (e: JSONException) {
+                Toast.makeText(baseContext, result, Toast.LENGTH_SHORT).show()
             }
         }
     }
