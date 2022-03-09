fea729e6b7777705f525e1acc2b42f5a012629d9
dkoukoul <dimitris.koukoulakis@gmail.com>
2022-03-09 12:14:37 +0200

    update peers list and added details on click

--- app/src/main/java/co/anode/anodium/wallet/WalletStatsActivity.kt
+++ app/src/main/java/co/anode/anodium/wallet/WalletStatsActivity.kt
@@ -20,6 +20,7 @@ import co.anode.anodium.R
 import co.anode.anodium.volley.APIController
 import co.anode.anodium.volley.ServiceVolley
 import org.json.JSONObject
+import kotlin.system.exitProcess
 
 class WalletStatsActivity : AppCompatActivity() {
     private var updating = true
@@ -30,6 +31,7 @@ class WalletStatsActivity : AppCompatActivity() {
     private var balanceLastTimeUpdated: Long = 0
     private var passwordPromptActive = false
     private lateinit var myBalance: TextView
+    val peersListDetails = mutableListOf<String>()
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -57,30 +59,31 @@ class WalletStatsActivity : AppCompatActivity() {
         //Initializing UI components
         val myaddress = findViewById<TextView>(R.id.wstats_address)
         myBalance = findViewById(R.id.wstats_balance)
-        val peersExpandbutton = findViewById<TextView>(R.id.peers_expand)
-        val bannedExpandbutton = findViewById<TextView>(R.id.banned_expand)
-        val queriesExpandbutton = findViewById<TextView>(R.id.queries_expand)
+        val peersExpandButton = findViewById<TextView>(R.id.peers_expand)
+        val bannedExpandButton = findViewById<TextView>(R.id.banned_expand)
+        val queriesExpandButton = findViewById<TextView>(R.id.queries_expand)
         val peersListView = findViewById<ListView>(R.id.peers_list)
         val bannedListView = findViewById<ListView>(R.id.banned_list)
         val queriesListView = findViewById<ListView>(R.id.queries_list)
-        peersListView.visibility = View.GONE
+        //Have peers list expanded
+        peersListView.visibility = View.VISIBLE
         bannedListView.visibility = View.GONE
         queriesListView.visibility = View.GONE
-        peersExpandbutton.setOnClickListener {
+        peersExpandButton.setOnClickListener {
             if (peersListView.visibility == View.VISIBLE) {
                 peersListView.visibility = View.GONE
             } else {
                 peersListView.visibility = View.VISIBLE
             }
         }
-        bannedExpandbutton.setOnClickListener {
+        bannedExpandButton.setOnClickListener {
             if (bannedListView.visibility == View.VISIBLE) {
                 bannedListView.visibility = View.GONE
             } else {
                 bannedListView.visibility = View.VISIBLE
             }
         }
-        queriesExpandbutton.setOnClickListener {
+        queriesExpandButton.setOnClickListener {
             if (queriesListView.visibility == View.VISIBLE) {
                 queriesListView.visibility = View.GONE
             } else {
@@ -94,20 +97,34 @@ class WalletStatsActivity : AppCompatActivity() {
             AnodeUtil.deleteNeutrino()
             AnodeUtil.stopPld()//will restart
         }
+        //Handle peer list click
+        peersListView.setOnItemClickListener { _, _, position, _ ->
+            showPeerDetails(peersListDetails[position])
+        }
     }
 
+    private fun showPeerDetails(details: String) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle("Peer Details")
+        builder.setMessage(details)
+        builder.setNegativeButton("OK") { dialog, _ ->
+            dialog.dismiss()
+        }
+        val alert: AlertDialog = builder.create()
+        alert.show()
+    }
     private fun getInfo() {
-        val walletsync = findViewById<TextView>(R.id.wstats_walletsync)
-        val neutrinosync = findViewById<TextView>(R.id.wstats_neutrinosync)
+        val walletSync = findViewById<TextView>(R.id.wstats_walletsync)
+        val neutrinoSync = findViewById<TextView>(R.id.wstats_neutrinosync)
         val connectedServers = findViewById<TextView>(R.id.wstats_connectedservers)
         val bannedServers = findViewById<TextView>(R.id.wstats_bannedservers)
-        val numqueries = findViewById<TextView>(R.id.wstats_queries)
+        val numQueries = findViewById<TextView>(R.id.wstats_queries)
         myBalance.text = "Requesting data"
-        walletsync.text = "Requesting data\n "
-        neutrinosync.text = "Requesting data\n "
+        walletSync.text = "Requesting data\n "
+        neutrinoSync.text = "Requesting data\n "
         connectedServers.text = "-"
         bannedServers.text = "-"
-        numqueries.text = "-"
+        numQueries.text = "-"
 
         val peersListView = findViewById<ListView>(R.id.peers_list)
         val bannedListView = findViewById<ListView>(R.id.banned_list)
@@ -117,9 +134,10 @@ class WalletStatsActivity : AppCompatActivity() {
             if (response != null) {
                 if (response.has("wallet") && !response.isNull("wallet")) {
                     val walletInfo = response.getJSONObject("wallet")
-                    walletsync.text = walletInfo.getInt("currentHeight").toString() + "\n" + walletInfo.getString("currentBlockTimestamp")
+                    walletSync.text = walletInfo.getInt("currentHeight").toString() + "\n" + walletInfo.getString("currentBlockTimestamp")
                 }
                 if (response.has("neutrino") && !response.isNull("neutrino")) {
+                    peersListDetails.clear()
                     val peersList = mutableListOf<String>()
                     val bansList = mutableListOf<String>()
                     val queriesList = mutableListOf<String>()
@@ -127,17 +145,20 @@ class WalletStatsActivity : AppCompatActivity() {
                     val bannedAdapter = ArrayAdapter(this,android.R.layout.simple_list_item_1, android.R.id.text1, bansList)
                     val queriesAdapter = ArrayAdapter(this,android.R.layout.simple_list_item_1, android.R.id.text1, queriesList)
                     val neutrino = response.getJSONObject("neutrino")
-                    neutrinosync.text = neutrino.getLong("height").toString() + "\n" + neutrino.getString("blockTimestamp")
+                    neutrinoSync.text = neutrino.getLong("height").toString() + "\n" + neutrino.getString("blockTimestamp")
                     connectedServers.text = neutrino.getJSONArray("peers").length().toString()
                     bannedServers.text = neutrino.getJSONArray("bans").length().toString()
-                    numqueries.text = neutrino.getJSONArray("queries").length().toString()
+                    numQueries.text = neutrino.getJSONArray("queries").length().toString()
                     val neutrinoPeers = neutrino.getJSONArray("peers")
                     for (i in 0 until neutrinoPeers.length()) {
                         val peerDesc = neutrinoPeers.getJSONObject(i)
-                        peersList.add("Address: " + peerDesc.getString("addr"))
-                        peersList.add("Version: " + peerDesc.getString("userAgent"))
-                        peersList.add("Sync: " + peerDesc.getString("lastBlock").toString())
+                        peersList.add("$i Address: " + peerDesc.getString("addr")+" Sync: " + peerDesc.getString("lastBlock").toString())
+                        peersListDetails.add("$i"+peerDesc.toString(i))
+//                        peersList.add("Version: " + peerDesc.getString("userAgent"))
+//                        peersList.add("Sync: " + peerDesc.getString("lastBlock").toString())
                     }
+                    peersList.sort()
+                    peersListDetails.sort()
                     for (i in 0 until neutrino.getJSONArray("bans").length()) {
                         val ban = neutrino.getJSONArray("bans").getJSONObject(i)
                         bansList.add("Address: " + ban.getString("addr"))
@@ -147,7 +168,7 @@ class WalletStatsActivity : AppCompatActivity() {
                     for (i in 0 until neutrino.getJSONArray("queries").length()) {
                         val query = neutrino.getJSONArray("queries").getJSONObject(i)
                         queriesList.add("Server: " + query.getString("peer"))
-                        queriesList.add("Request: " + query.getString("command"))
+//                        queriesList.add("Request: " + query.getString("command"))
                         queriesList.add("Created: " + query.getString("createTime"))
                         if (query.getLong("lastResponseTime") > 0) {
                             val datetime = Date(query.getLong("lastResponseTime"))
@@ -160,7 +181,6 @@ class WalletStatsActivity : AppCompatActivity() {
                     bannedListView.adapter = bannedAdapter
                     queriesListView.adapter = queriesAdapter
                 }
-
             }
             h.postDelayed(getPldInfo, refreshValuesInterval)
         }
