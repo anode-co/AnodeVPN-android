97b7804a5091f63442c8ee394ea84338c20750d9
dkoukoul <dimitris.koukoulakis@gmail.com>
2022-03-15 09:37:38 +0200

    update with limited lines of pld and change background to black

--- app/src/main/java/co/anode/anodium/wallet/DebugWalletActivity.kt
+++ app/src/main/java/co/anode/anodium/wallet/DebugWalletActivity.kt
@@ -1,9 +1,11 @@
 package co.anode.anodium.wallet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.ScrollView
 import android.widget.TextView
 import co.anode.anodium.support.AnodeUtil
@@ -12,9 +14,7 @@ import co.anode.anodium.R
 import java.io.File
 
 class DebugWalletActivity : AppCompatActivity() {
-    private var scrollposition = 0
     private var toBottom = false
-    private var logfile = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,32 +28,37 @@ class DebugWalletActivity : AppCompatActivity() {
         //Open pld log file and display it
         val logtext = findViewById<TextView>(R.id.debugwalletlogtext)
 
-        logfile = File(AnodeUtil.CJDNS_PATH +"/"+ AnodeUtil.PLD_LOG).readText()
-        logtext.text = logfile
+        val logFile = File(AnodeUtil.CJDNS_PATH +"/"+ AnodeUtil.PLD_LOG)
+        val maxLength = 500*1024
+        if (logFile.length() > maxLength) {
+            logtext.text = logFile.readText().drop(logFile.length().toInt()-maxLength)
+        } else {
+            logtext.text = logFile.readText()
+        }
+
         val scroll = findViewById<ScrollView>(R.id.wallet_debug_scroll)
         scroll.post {
             scroll.fullScroll(View.FOCUS_DOWN)
         }
-
         scroll.setOnScrollChangeListener { _, _, scrollY, _, _ ->
-            scrollposition = scrollY
             val diff = scroll.getChildAt(0).bottom - (scrollY + scroll.height)
             toBottom = diff < 50
         }
-
         Thread({
             Log.i(LOGTAG, "DebugActivity.RefreshValues startup")
             var sleep: Long = 500
-            var oldlog = logfile
+            var oldLog = logtext.text
             while (true) {
                 this.runOnUiThread {
                     if (toBottom) {
                         val newlog = File(AnodeUtil.CJDNS_PATH + "/" + AnodeUtil.PLD_LOG).readText()
-                        if (newlog.length > oldlog.length) {
-                            oldlog = newlog
-                            logtext.text = newlog
-                            //position is in characters?
-                            //scroll.fullScroll(View.FOCUS_DOWN)
+                        if (newlog.length > oldLog.length) {
+                            oldLog = newlog
+                            if (newlog.length > maxLength) {
+                                logtext.text = newlog.drop(logFile.length().toInt()-maxLength)
+                            } else {
+                                logtext.text = newlog
+                            }
                             scroll.post {
                                 scroll.fullScroll(View.FOCUS_DOWN)
                             }
@@ -68,6 +73,16 @@ class DebugWalletActivity : AppCompatActivity() {
                 Thread.sleep(sleep)
             }
         }, "DebugWalletActivity.RefreshValues").start()
+        val shareButton = findViewById<Button>(R.id.buttonSharePldLog)
+        shareButton.setOnClickListener {
+            val sendIntent: Intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, File(AnodeUtil.CJDNS_PATH + "/" + AnodeUtil.PLD_LOG).readText())
+                type = "text/plain"
+            }
+            val shareIntent = Intent.createChooser(sendIntent, null)
+            startActivity(shareIntent)
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
