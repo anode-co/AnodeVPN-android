23b0a75a09237ec2dbcf4d21db9f26b607e6beaf
dkoukoul <dimitris.koukoulakis@gmail.com>
2022-03-22 17:27:44 +0200

    handling pld errors

--- app/src/main/java/co/anode/anodium/volley/ServiceVolley.kt
+++ app/src/main/java/co/anode/anodium/volley/ServiceVolley.kt
@@ -1,6 +1,7 @@
 package co.anode.anodium.volley
 
 import android.util.Log
+import co.anode.anodium.support.AnodeUtil
 import com.android.volley.AuthFailureError
 import com.android.volley.DefaultRetryPolicy
 import com.android.volley.Response
@@ -19,13 +20,19 @@ class ServiceVolley : ServiceInterface {
             },
             Response.ErrorListener { error ->
                 VolleyLog.e(TAG, "/post request fail! Error: ${error.message}")
+                val jsonError = JSONObject()
                 if ((error.networkResponse != null) && (error.networkResponse.data != null )) {
                     val errorString = String(error.networkResponse.data)
-                    val jsonError = JSONObject()
                     jsonError.put("error", errorString)
                     completionHandler(jsonError)
+                } else if ((!error.message.isNullOrEmpty()) && (error.message!!.contains("java.net.ConnectException: Failed to connect to localhost/127.0.0.1:8080"))) {
+                    //pls is not running try to launch it
+                    AnodeUtil.launchPld()
+                    jsonError.put("error", "pld not running")
+                    completionHandler(jsonError)
                 } else {
-                    completionHandler(JSONObject("{}"))
+                    jsonError.put("error", "unknown")
+                    completionHandler(jsonError)
                 }
             }) {}
         jsonObjReq.retryPolicy = DefaultRetryPolicy(4000, 0, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT)
@@ -40,15 +47,22 @@ class ServiceVolley : ServiceInterface {
             },
             Response.ErrorListener { error ->
                 VolleyLog.e(TAG, "/post request fail! Error: ${error.message}")
+                val jsonError = JSONObject()
                 if ((error.networkResponse != null) && (error.networkResponse.data != null)) {
                     val errorString = String(error.networkResponse.data)
-                    val jsonError = JSONObject()
                     jsonError.put("error", errorString)
                     completionHandler(jsonError)
+                } else if ((!error.message.isNullOrEmpty()) && (error.message!!.contains("java.net.ConnectException: Failed to connect to localhost/127.0.0.1:8080"))) {
+                    //pls is not running try to launch it
+                    AnodeUtil.launchPld()
+                    jsonError.put("error", "pld not running")
+                    completionHandler(jsonError)
+                } else if ((!error.message.isNullOrEmpty()) && (error.message!!.contains("End of input at character 0"))) {
+                    //handle empty response indicating recovery worked
+                    completionHandler(jsonError)
                 } else {
-                    val jsonError = JSONObject()
                     jsonError.put("error", "unknown")
-                    completionHandler(JSONObject("{}"))
+                    completionHandler(jsonError)
                 }
             }) {
             @Throws(AuthFailureError::class)
@@ -59,11 +73,13 @@ class ServiceVolley : ServiceInterface {
             }
         }
         //Increase timeout when we are creating a wallet
-        if ((url.contains("wallet/create")) || (url.contains("sendfrom"))) {
+        if (url.contains("wallet/create"))  {
+            jsonObjReq.retryPolicy = DefaultRetryPolicy(60000, 0, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT)
+        } else if ((url.contains("sendfrom")) || (url.contains("seed/create"))) {
             jsonObjReq.retryPolicy = DefaultRetryPolicy(40000, 0, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT)
         } else if (url.contains("wallet/unlock")) {
             jsonObjReq.retryPolicy = DefaultRetryPolicy(15000, 0, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT)
-        } else{
+        } else {
             jsonObjReq.retryPolicy = DefaultRetryPolicy(4000, 0, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT)
         }
         BackendVolley.instance?.addToRequestQueue(jsonObjReq, TAG)
