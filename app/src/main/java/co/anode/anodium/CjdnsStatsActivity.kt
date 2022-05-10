9c3761f95a7420ea01dd89bd82ed02fa13234daf
dkoukoul <dimitris.koukoulakis@gmail.com>
2022-05-10 09:34:41 +0300

    checking if cjdns is running

--- app/src/main/java/co/anode/anodium/CjdnsStatsActivity.kt
+++ app/src/main/java/co/anode/anodium/CjdnsStatsActivity.kt
@@ -26,7 +26,7 @@ class CjdnsStatsActivity : AppCompatActivity() {
         //actionbar
         val actionbar = supportActionBar
         //set actionbar title
-        actionbar!!.title = "View Logs"
+        actionbar!!.title = "Cjdns Stats"
         //set back button
         actionbar.setDisplayHomeAsUpEnabled(true)
         val prefs = getSharedPreferences("co.anode.anodium", Context.MODE_PRIVATE)
@@ -55,46 +55,53 @@ class CjdnsStatsActivity : AppCompatActivity() {
         Thread({
             Log.i(LOGTAG, "DebugActivity.RefreshValues startup")
             while (true) {
-                this.runOnUiThread {
-                    val appversion = findViewById<TextView>(R.id.text_AppVersion)
-                    appversion.text = "App Version: " + BuildConfig.VERSION_NAME
-                    val ipv4 = findViewById<TextView>(R.id.text_ipv4)
-                    ipv4.text = CjdnsSocket.ipv4Address
-                    val cjdnsipv6 = findViewById<TextView>(R.id.text_cjdnsipv6)
-                    val nodeinfo = CjdnsSocket.Core_nodeInfo()
-                    cjdnsipv6.text = nodeinfo["myIp6"].str()
-                    val internetipv6 = findViewById<TextView>(R.id.text_internetipv6)
-                    internetipv6.text = CjdnsSocket.ipv6Address
-                    val pubkey = findViewById<TextView>(R.id.text_pubkey)
-                    pubkey.text = AnodeUtil.getPubKey()
-                    val nodeLink = findViewById<TextView>(R.id.text_nodes)
-                    val link: Spanned = HtmlCompat.fromHtml("<a href='http://h.snode.cjd.li/#" + nodeinfo["myIp6"].str() + "'>Find yourself on the map</a>", HtmlCompat.FROM_HTML_MODE_LEGACY)
-                    nodeLink.movementMethod = LinkMovementMethod.getInstance()
-                    nodeLink.text = link
-                    val username = findViewById<TextView>(R.id.text_username)
-                    username.text = prefs.getString("username", "")
-                    val node = findViewById<TextView>(R.id.text_node)
-                    node.text = prefs.getString("ServerPublicKey", "")
-                    val peerstats = findViewById<TextView>(R.id.text_peerstats)
-                    val peers: ArrayList<Benc.Bdict> = CjdnsSocket.InterfaceController_peerStats()
-                    var info = ""
-                    for (i in 0 until peers.count()) {
-                        info += peers[i]["lladdr"].toString() + " " + peers[i]["addr"].toString() + " " + peers[i]["state"].toString() + " in " + peers[i]["recvKbps"].toString() + " out " + peers[i]["sendKbps"].toString() + " " + peers[i]["user"].toString() + "\n"
-                    }
-                    peerstats.text = info
+                if (AnodeUtil.isCjdnsAlive()) {
+                    this.runOnUiThread {
+                        val appversion = findViewById<TextView>(R.id.text_AppVersion)
+                        appversion.text = "App Version: " + BuildConfig.VERSION_NAME
+                        val ipv4 = findViewById<TextView>(R.id.text_ipv4)
+                        ipv4.text = CjdnsSocket.ipv4Address
+                        val cjdnsipv6 = findViewById<TextView>(R.id.text_cjdnsipv6)
+                        val nodeinfo = CjdnsSocket.Core_nodeInfo()
+                        cjdnsipv6.text = nodeinfo["myIp6"].str()
+                        val internetipv6 = findViewById<TextView>(R.id.text_internetipv6)
+                        internetipv6.text = CjdnsSocket.ipv6Address
+                        val pubkey = findViewById<TextView>(R.id.text_pubkey)
+                        pubkey.text = AnodeUtil.getPubKey()
+                        val nodeLink = findViewById<TextView>(R.id.text_nodes)
+                        val link: Spanned = HtmlCompat.fromHtml("<a href='http://h.snode.cjd.li/#" + nodeinfo["myIp6"].str() + "'>Find yourself on the map</a>", HtmlCompat.FROM_HTML_MODE_LEGACY)
+                        nodeLink.movementMethod = LinkMovementMethod.getInstance()
+                        nodeLink.text = link
+                        val username = findViewById<TextView>(R.id.text_username)
+                        username.text = prefs.getString("username", "")
+                        val node = findViewById<TextView>(R.id.text_node)
+                        node.text = prefs.getString("ServerPublicKey", "")
+                        val peerstats = findViewById<TextView>(R.id.text_peerstats)
+                        val peers: ArrayList<Benc.Bdict> = CjdnsSocket.InterfaceController_peerStats()
+                        var info = ""
+                        for (i in 0 until peers.count()) {
+                            info += peers[i]["lladdr"].toString() + " " + peers[i]["addr"].toString() + " " + peers[i]["state"].toString() + " in " + peers[i]["recvKbps"].toString() + " out " + peers[i]["sendKbps"].toString() + " " + peers[i]["user"].toString() + "\n"
+                        }
+                        peerstats.text = info
 
-                    val listconnnections = findViewById<TextView>(R.id.text_listconnections)
-                    val connections: ArrayList<Benc.Bdict> = CjdnsSocket.IpTunnel_listConnections()
-                    info = ""
-                    for (i in 0 until connections.count()) {
-                        info += connections[i].toString()
+                        val listconnnections = findViewById<TextView>(R.id.text_listconnections)
+                        val connections: ArrayList<Benc.Bdict> = CjdnsSocket.IpTunnel_listConnections()
+                        info = ""
+                        for (i in 0 until connections.count()) {
+                            info += connections[i].toString()
+                        }
+                        listconnnections.text = info
+                        val publicip = findViewById<TextView>(R.id.text_publicIP)
+                        publicip.text = "updating ip..."
+                        GetPublicIP().execute(publicip)
                     }
-                    listconnnections.text = info
-                    val publicip = findViewById<TextView>(R.id.text_publicIP)
-                    publicip.text = "updating ip..."
-                    GetPublicIP().execute(publicip)
+                    Thread.sleep(8000)
+                } else {
+                    runOnUiThread {
+                        Toast.makeText(this,"Cjdns is not running.", Toast.LENGTH_LONG).show()
+                    }
+                    return@Thread
                 }
-                Thread.sleep(8000)
             }
         }, "DebugActivity.RefreshValues").start()
     }
