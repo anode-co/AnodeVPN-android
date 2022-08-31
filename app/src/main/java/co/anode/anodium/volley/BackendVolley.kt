8324ffdf5ce069cd8ea269e8acab8fdc337e9928
dkoukoul <dimitris.koukoulakis@gmail.com>
2022-08-31 13:41:47 +0300

    bug fix

--- app/src/main/java/co/anode/anodium/volley/BackendVolley.kt
+++ app/src/main/java/co/anode/anodium/volley/BackendVolley.kt
@@ -7,33 +7,25 @@ import com.android.volley.RequestQueue
 import com.android.volley.toolbox.Volley
 
 class BackendVolley : Application() {
+    private lateinit var mRequestQueue: RequestQueue
     override fun onCreate() {
         super.onCreate()
         instance = this
+        mRequestQueue = Volley.newRequestQueue(applicationContext)
     }
-
-    val requestQueue: RequestQueue? = null
-        get() {
-            if (field == null) {
-                return Volley.newRequestQueue(applicationContext)
-            }
-            return field
-        }
 
     fun <T> addToRequestQueue(request: Request<T>, tag: String) {
         request.tag = if (TextUtils.isEmpty(tag)) TAG else tag
-        requestQueue?.add(request)
+        mRequestQueue.add(request)
     }
 
     fun <T> addToRequestQueue(request: Request<T>) {
         request.tag = TAG
-        requestQueue?.add(request)
+        mRequestQueue.add(request)
     }
 
     fun cancelPendingRequests(tag: Any) {
-        if (requestQueue != null) {
-            requestQueue!!.cancelAll(tag)
-        }
+        mRequestQueue.cancelAll(tag)
     }
 
     companion object {
