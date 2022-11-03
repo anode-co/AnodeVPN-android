7c0b1aab2dd47e59482a31284b06095a33621088
dkoukoul <dimitris.koukoulakis@gmail.com>
2022-11-03 17:02:41 +0200

    restructured walletinfo and added functionality

--- pkt/src/main/java/com/pkt/core/presentation/main/wallet/WalletViewModel.kt
+++ pkt/src/main/java/com/pkt/core/presentation/main/wallet/WalletViewModel.kt
@@ -1,6 +1,5 @@
 package com.pkt.core.presentation.main.wallet
 
-import android.content.Intent
 import androidx.lifecycle.SavedStateHandle
 import com.pkt.core.presentation.common.adapter.DisplayableItem
 import com.pkt.core.presentation.common.state.StateViewModel
@@ -9,6 +8,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import java.time.LocalDateTime
 import java.time.ZonedDateTime
 import com.pkt.core.extensions.*
+import java.text.SimpleDateFormat
 import javax.inject.Inject
 import kotlin.math.absoluteValue
 
@@ -24,100 +24,150 @@ class WalletViewModel @Inject constructor(
         ?: throw IllegalArgumentException("PKTtoUSD is required")
 
 
+
     init {
         invokeLoadingAction {
             runCatching {
-                val walletInfo = walletRepository.getWalletInfo().getOrThrow()
-                var address = ""
-                if (walletInfo.wallet == null) {
-                    //Wallet is locked
-                    //TODO: bring up enterwallet fragment
-                } else {
-                    address = walletRepository.getWalletAddress().getOrThrow()
-                    //If no address, then create one
-                    if (address.isEmpty()) {
-                        val response = walletRepository.createAddress().getOrThrow()
-                        address = response.address
-                    }
-                }
-                val balance = walletRepository.getTotalWalletBalance().getOrThrow()
-                Triple(walletInfo, address, balance)
-            }.onSuccess { (info, address, balance) ->
-                val wallet = info.wallet
-                val neutrino = info.neutrino
-                val peerCount = neutrino.peers.size
-                val neutrinoHeight = neutrino.height
-                var neutrinoTop = neutrinoHeight
-                for (i in 0 until peerCount) {
-                    if (neutrino.peers[i].lastBlock > neutrinoTop)
-                        neutrinoTop = neutrino.peers[i].lastBlock
-                }
-
-                sendState {
-                    copy(
-                        syncState = WalletState.SyncState.SUCCESS,
-                        peersCount = peerCount,
-                        block = "$neutrinoHeight/$neutrinoTop",
-                        walletName = walletName,
-                        balancePkt = balance.toLong().toString(),
-                        balanceUsd = balance.toLong().toPKT().multiply(PKTtoUSD.toBigDecimal()).toString(),
-                        walletAddress = address,
-                        items = listOf()
-                    )
-                }
-                //Get transactions
-                runCatching {
-                    walletRepository.getWalletTransactions().getOrThrow()
-                }.onSuccess { t ->
-                    val transactions = t.transactions
-                    val items: MutableList<DisplayableItem> = mutableListOf()
-                    var prevDate: LocalDateTime = LocalDateTime.ofEpochSecond(0, 0, ZonedDateTime.now().offset)
-                    for (i in transactions.indices) {
-                        //Add date
-                        val date = LocalDateTime.ofEpochSecond(transactions[i].timeStamp.toLong(), 0, ZonedDateTime.now().offset)
-                        if ((prevDate.dayOfMonth != date.dayOfMonth) ||
-                            (prevDate.month != date.month) ||
-                            (prevDate.year != date.year)
-                        ) {
-                            items.add(DateItem(date))
-                        }
-                        prevDate = date
-
-                        var amount = transactions[i].amount.toLong()
-                        var type = TransactionItem.Type.RECEIVE
-                        if (amount < 0)
-                            type = TransactionItem.Type.SENT
-                        amount = amount.absoluteValue
-                        items.add(
-                            TransactionItem(
-                                id = i.toString(),
-                                type = type,
-                                time = date,
-                                amountPkt = amount.toString(),
-                                amountUsd = amount.toPKT().multiply(PKTtoUSD.toBigDecimal()).toString(),
-                            )
-                        )
-                    }
-                    items.add(FooterItem())
-                    sendState {
-                        copy(
-                            syncState = WalletState.SyncState.SUCCESS,
-                            items = items,
-                        )
-                    }
-                }.onFailure {
-
-                }
+                loadWalletInfo()
+            }.onSuccess {
+                loadTransactions()
             }.onFailure {
-                //TODO: identify error, check for unlock
+                //WalletInfo failed, try to unlock
+                //TODO: check for error
                 walletRepository.unlockWallet("password")
             }
         }
     }
 
+    private suspend fun loadWalletInfo(): Result<Boolean>{
+        runCatching {
+            val walletInfo = walletRepository.getWalletInfo().getOrThrow()
+            var address = ""
+            if (walletInfo.wallet == null) {
+                //Wallet is locked
+                //TODO: bring up enterwallet fragment
+            } else {
+                address = walletRepository.getWalletAddress().getOrThrow()
+                //If no address, then create one
+                if (address.isEmpty()) {
+                    val response = walletRepository.createAddress().getOrThrow()
+                    address = response.address
+                }
+            }
+            val balance = walletRepository.getTotalWalletBalance().getOrThrow()
+            Triple(walletInfo, address, balance)
+        }.onSuccess { (info, address, balance) ->
+            val wallet = info.wallet
+            val neutrino = info.neutrino
+            val peerCount = neutrino.peers.size
+            val neutrinoHeight = neutrino.height
+            var neutrinoTop = neutrinoHeight
+            var syncTimeDiff: Long = 0
+            var walletSyncState = WalletState.SyncState.FAILED
+            for (i in 0 until peerCount) {
+                if (neutrino.peers[i].lastBlock > neutrinoTop) {
+                    neutrinoTop = neutrino.peers[i].lastBlock
+                }
+            }
+
+            //Set wallet status
+            if (peerCount == 0){
+                walletSyncState = WalletState.SyncState.FAILED
+            } else if (neutrinoHeight < neutrinoTop) {
+                walletSyncState = WalletState.SyncState.DOWNLOADING
+            } else if (wallet?.currentHeight!! < neutrinoHeight) {
+                walletSyncState = WalletState.SyncState.SCANNING
+            } else if (wallet.currentHeight == neutrinoHeight) {
+                walletSyncState = WalletState.SyncState.SUCCESS
+                val timestamp = neutrino.blockTimestamp
+                var bTimestamp: Long = 0
+                if (!timestamp.isNullOrEmpty()) {
+                    bTimestamp = SimpleDateFormat("yyyy-MM-dd HH:mm:ss z").parse(timestamp).time
+                }
+                syncTimeDiff = (System.currentTimeMillis() - bTimestamp) / 1000
+                if (syncTimeDiff > 1440) {
+                    walletSyncState = WalletState.SyncState.WAITING
+                }
+            }
+
+            neutrinoTop = 0
+            sendState {
+                copy(
+                    syncState = walletSyncState,
+                    syncTimeDiff = syncTimeDiff,
+                    chainHeight = neutrinoHeight,
+                    neutrinoTop = neutrinoTop,
+                    walletHeight = walletHeight,
+                    peersCount = peerCount,
+                    block = "$neutrinoHeight/$neutrinoTop",
+                    walletName = walletName,
+                    balancePkt = balance.toLong().toString(),
+                    balanceUsd = balance.toLong().toPKT().multiply(PKTtoUSD.toBigDecimal()).toString(),
+                    walletAddress = address,
+                    items = listOf()
+                )
+            }
+            return Result.success(true)
+        }.onFailure {
+            //TODO: handle error
+            return Result.failure(it)
+        }
+        return Result.success(true)
+    }
+
+    private suspend fun loadTransactions(){
+        //Get transactions
+        runCatching {
+            walletRepository.getWalletTransactions().getOrThrow()
+        }.onSuccess { t ->
+            val transactions = t.transactions
+            val items: MutableList<DisplayableItem> = mutableListOf()
+            var prevDate: LocalDateTime = LocalDateTime.ofEpochSecond(0, 0, ZonedDateTime.now().offset)
+            for (i in transactions.indices) {
+                //Add date
+                val date = LocalDateTime.ofEpochSecond(transactions[i].timeStamp.toLong(), 0, ZonedDateTime.now().offset)
+                if ((prevDate.dayOfMonth != date.dayOfMonth) ||
+                    (prevDate.month != date.month) ||
+                    (prevDate.year != date.year)
+                ) {
+                    items.add(DateItem(date))
+                }
+                prevDate = date
+
+                var amount = transactions[i].amount.toLong()
+                var type = TransactionItem.Type.RECEIVE
+                if (amount < 0)
+                    type = TransactionItem.Type.SENT
+                amount = amount.absoluteValue
+                items.add(
+                    TransactionItem(
+                        id = i.toString(),
+                        type = type,
+                        time = date,
+                        amountPkt = amount.toString(),
+                        amountUsd = amount.toPKT().multiply(PKTtoUSD.toBigDecimal()).toString(),
+                    )
+                )
+            }
+            items.add(FooterItem())
+            sendState {
+                copy(
+                    syncState = WalletState.SyncState.SUCCESS,
+                    items = items,
+                )
+            }
+        }.onFailure {
+            //TODO: handle failure
+        }
+    }
+
     override fun createInitialState(): WalletState {
         return WalletState(
-            syncState = WalletState.SyncState.SUCCESS,
+            syncState = WalletState.SyncState.WAITING,
+            chainHeight = 0,
+            neutrinoTop = 0,
+            syncTimeDiff = 0,
+            walletHeight = 0,
             peersCount = 0,
             block = "",
             walletName = walletName,
