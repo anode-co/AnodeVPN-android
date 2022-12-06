1f12c0eacf4cb7aa30d26dce53415f049156837b
dkoukoul <dimitris.koukoulakis@gmail.com>
2022-12-06 11:16:16 +0200

    added webview related functions

--- app/src/main/java/co/anode/anodium/integration/model/repository/GeneralRepositoryImpl.kt
+++ app/src/main/java/co/anode/anodium/integration/model/repository/GeneralRepositoryImpl.kt
@@ -3,6 +3,8 @@ package co.anode.anodium.integration.model.repository
 import co.anode.anodium.support.AnodeClient
 import co.anode.anodium.support.AnodeUtil
 import com.pkt.domain.repository.GeneralRepository
+import timber.log.Timber
+import java.io.File
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -30,15 +32,43 @@ class GeneralRepositoryImpl @Inject constructor() : GeneralRepository {
     }
 
     override fun removePIN(walletName: String) {
+        Timber.d("removePIN $walletName")
         AnodeUtil.removeEncryptedWalletPreferences(walletName)
     }
 
     override fun restartApplication() {
+        Timber.d("restartApplication")
         Thread.sleep(500)
         AnodeUtil.restartApp()
     }
 
     override fun hasInternetConnection(): Boolean {
         return AnodeUtil.internetConnection()
+    }
+
+    override fun getDataConsent(): Boolean {
+        return AnodeUtil.getDataConsentFromSharedPrefs()
+    }
+
+    override fun setDataConsent(value: Boolean) {
+        AnodeUtil.setDataConsentToSharedPrefs(value)
+    }
+
+    override fun saveGetInfoHtml(html: String) {
+        val htmlFile = File("${AnodeUtil.filesDirectory}/getinfo.html")
+        htmlFile.writeText(html, Charsets.UTF_8)
+    }
+
+    override fun savePldLogHtml(html: String) {
+        val htmlFile = File("${AnodeUtil.filesDirectory}/pldlog.html")
+        htmlFile.writeText(html, Charsets.UTF_8)
+    }
+
+    override fun getWalletInfoUrl(): String {
+        return "file:///${AnodeUtil.filesDirectory}/getinfo.html"
+    }
+
+    override fun getPldLogUrl(): String {
+        return "file:///${AnodeUtil.filesDirectory}/pldlog.html"
     }
 }
