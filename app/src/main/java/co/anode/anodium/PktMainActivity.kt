834bccc89a5e4c857f6e582889fd81d40d71ec73
dkoukoul <dimitris.koukoulakis@gmail.com>
2023-01-04 17:10:46 +0200

    added permission checking on pkt activity

--- app/src/main/java/co/anode/anodium/PktMainActivity.kt
+++ app/src/main/java/co/anode/anodium/PktMainActivity.kt
@@ -1,15 +1,53 @@
 package co.anode.anodium
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
 
 @AndroidEntryPoint
 class PktMainActivity : AppCompatActivity() {
+    private val REQUIRED_SDK_PERMISSIONS = arrayOf(
+        Manifest.permission.ACCESS_NETWORK_STATE,
+        Manifest.permission.ACCESS_WIFI_STATE,
+        Manifest.permission.INTERNET,
+        Manifest.permission.CHANGE_NETWORK_STATE,
+        Manifest.permission.CHANGE_WIFI_STATE,
+        Manifest.permission.BIND_VPN_SERVICE
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pkt_main)
 
+        checkPermissions()
+    }
 
+    private fun checkPermissions() {
+        val REQUEST_CODE_ASK_PERMISSIONS = 1
+        val missingPermissions: MutableList<String> = ArrayList()
+        // check all required dynamic permissions
+        for (permission in REQUIRED_SDK_PERMISSIONS) {
+            val result = ContextCompat.checkSelfPermission(this, permission)
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                missingPermissions.add(permission)
+            }
+        }
+        if (missingPermissions.isNotEmpty()) {
+            // request all missing permissions
+            val permissions = missingPermissions.toTypedArray()
+            ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE_ASK_PERMISSIONS)
+        } else {
+            val grantResults = IntArray(REQUIRED_SDK_PERMISSIONS.size)
+            Arrays.fill(grantResults, PackageManager.PERMISSION_GRANTED)
+            onRequestPermissionsResult(
+                REQUEST_CODE_ASK_PERMISSIONS, REQUIRED_SDK_PERMISSIONS,
+                grantResults
+            )
+        }
     }
 }
